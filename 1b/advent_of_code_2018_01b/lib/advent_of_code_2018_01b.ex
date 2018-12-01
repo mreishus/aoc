1de6149a27fd1827df9a8c103ed3ed50bec23464
Matthew Reishus <mreishus@users.noreply.github.com>
2018-12-01 23:12:04 +0000

    Cleanup 1b, add tests

--- 1b/advent_of_code_2018_01b/lib/advent_of_code_2018_01b.ex
+++ 1b/advent_of_code_2018_01b/lib/advent_of_code_2018_01b.ex
@@ -2,17 +2,17 @@ defmodule AdventOfCode201801b do
   @moduledoc """
     Author: Matthew Reishus
     Date: 2018-12-01
-    Purpose: Advent Of Code 2018, Day 1, Puzzle 1
+    Purpose: Advent Of Code 2018, Day 1, Puzzle 2
 
     Program reads a text file in "input.txt" comprised of lines like
     "+5", "-100" or "+89". All lines begin with either "-" or "+".
 
-    It returns the sum of all of the numbers.  For example, a file containing
-    "+1", "+2", "+100", "-50" will return "53".
+    The program returns the first sum that is reached twice.  It may have to 
+    loop over the entire list.
   """
 
   @doc """
-    sum_strings_with_operator: Meant to be passed to a Enum.reduce call.
+    sum_strings_with_operator: Meant to be passed to a Enum.reduce call, accumulating a rolling sum.
 
     Param 1: string_with_operator (string) formatted like "+1" or "-10".  Sign is mandatory.
     Param 2: sum (integer) Rolling sum.
@@ -27,6 +27,17 @@ defmodule AdventOfCode201801b do
     end
   end
 
+
+  @doc """
+    calculate_tally: Meant to be passed to a Enum.reduce call. This uses a map called "tally"
+    as the accumulator, which contains both the current sum and the previous sums we have seen.
+
+    Param 1: string_with_operator (string) formatted like "+1" or "-10".  Sign is mandatory.
+    Param 2: tally (map) with these keys:
+      sum (integer) Rolling sum.
+      seen_map (map -> %{integer: integer}) Which sums we have seen and how many times.
+      seen_multiple (list of integers) All sums seen multiple times, the most recent at end of the list.
+  """
   def calculate_tally(string_with_operator, tally) do
     sum = sum_strings_with_operator(string_with_operator, tally.sum)
 
@@ -42,25 +53,9 @@ defmodule AdventOfCode201801b do
   end
 
   @doc """
-    compute_input_sum: Reads all lines in input.txt, sums them, and prints the
-    sum to console.
+    compute_input_sum: Reads all lines in input.txt, summing them, and finds the
+    first repeated sum, repeating the file as many times as possible.
   """
-  def czompute_input_sum() do
-    file_name = Path.expand("./", __DIR__) |> Path.join("input.txt")
-
-    tally = %{
-      sum: 0,
-      seen_map: %{},
-      seen_multiple: [],
-    }
-
-    tally = File.stream!(file_name)
-      |> Stream.map(&String.trim/1)
-      |> Enum.reduce(tally, &AdventOfCode201801b.calculate_tally/2)
-
-    IO.inspect tally, charlists: :as_lists
-  end
-
   def compute_input_sum() do
     file_name = Path.expand("./", __DIR__) |> Path.join("input.txt")
     list = File.stream!(file_name)
@@ -69,16 +64,20 @@ defmodule AdventOfCode201801b do
     IO.inspect f
   end
 
+  @doc """
+    first_repeated_sum: Given a list of strings like "+1", "-5", "+20", find the first repeated sum,
+    repeating the entire list as many times as needed.  May run forever.
+  """
   def first_repeated_sum(list) do
     tally = %{
       sum: 0,
-      seen_map: %{},
+      seen_map: %{0 => 1},
       seen_multiple: [],
     }
     first_repeated_sum(list, tally)
   end
 
-  def first_repeated_sum(list, tally) do
+  defp first_repeated_sum(list, tally) do
     tally = list |> Enum.reduce(tally, &AdventOfCode201801b.calculate_tally/2)
 
     case length(tally.seen_multiple) do
@@ -86,6 +85,5 @@ defmodule AdventOfCode201801b do
       _ -> List.first(tally.seen_multiple)
     end
   end
-
 end
 
--- 1b/advent_of_code_2018_01b/test/advent_of_code_2018_01b_test.exs
+++ 1b/advent_of_code_2018_01b/test/advent_of_code_2018_01b_test.exs
@@ -14,5 +14,21 @@ defmodule AdventOfCode201801bTest do
     assert AdventOfCode201801b.sum_strings_with_operator("+0", 100) == 100
     assert AdventOfCode201801b.sum_strings_with_operator("-0", 100) == 100
   end
+
+  test "first_repeated_sum works without replaying the list" do
+    list = [ "+1", "+1", "+1", "+1", "+1", "+1", "+1", "+1", "+1", "-5", "+1", "+1" ]
+    # Should sum up to 9, then remove 5 = 4 is first duplicate
+    assert AdventOfCode201801b.first_repeated_sum(list) == 4
+  end
+
+  test "first_repeated_sum works with replaying the list" do
+    list = [ "+10", "+10", "+10", "+1", "+1", "+1", "+1", "+1", "+10", "-30" ]
+    assert AdventOfCode201801b.first_repeated_sum(list) == 35
+  end
+
+  test "first_repeated_sum works with replaying the list, and counts 0 as seen" do
+    list = [ "+1", "+1", "+1", "+1", "+1", "-6"]
+    assert AdventOfCode201801b.first_repeated_sum(list) == 0
+  end
 end
 
