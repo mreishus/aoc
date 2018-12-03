1983ad4a9b16b27ed65a090cc6471e1d4f077fd4
Matthew Reishus <mreishus@users.noreply.github.com>
2018-12-03 05:24:34 +0000

    Rough ruby solution to 3a

---
+++ 3/rough-ruby/3.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+require 'pp'
+
+BOARD_SIZE = 1500
+#BOARD_SIZE = 11
+
+board = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE, 0) }
+File.readlines('input.txt').each do |line|
+  re = '#(\d+) @ (\d+),(\d+): (\d+)x(\d+)'
+  _, id, x, y, width, length = line.match(re).to_a
+
+  puts "#{line}"
+  puts "id #{id} x#{x} y#{y} width#{width} length#{length}"
+
+  id = id.to_i
+  x = x.to_i
+  y = y.to_i
+  width = width.to_i
+  length = length.to_i
+
+  puts x + width
+  x.upto(x + width - 1) do |xx|
+    y.upto(y + length - 1) do |yy|
+      #puts "    #{xx},#{yy} add one"
+      board[xx][yy] += 1
+    end
+  end
+end
+
+count = 0;
+0.upto(BOARD_SIZE - 1) do |x|
+  0.upto(BOARD_SIZE - 1) do |y|
+    if (board[x][y] > 1)
+      #puts "    #{x},#{y} = #{board[x][y]}"
+      count += 1
+    end
+  end
+end
+
+puts count
