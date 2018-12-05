8cc939bb03e6639ef698bab215abd49cf8f96239
Matthew Reishus <mreishus@users.noreply.github.com>
2018-12-05 03:34:14 +0000

    Day 4, part 1 working

--- 4/advent_of_code_2018_04/lib/advent_of_code_2018_04.ex
+++ 4/advent_of_code_2018_04/lib/advent_of_code_2018_04.ex
@@ -1,23 +1,6 @@
 defmodule AdventOfCode201804 do
-  @moduledoc """
-  Documentation for AdventOfCode201804.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AdventOfCode201804.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def read_file() do
-    file_name = Path.expand("./", __DIR__) |> Path.join("input_small_unsorted.txt")
+    file_name = Path.expand("./", __DIR__) |> Path.join("input_small.txt")
     {:ok, contents} = File.read(file_name)
     contents
       |> String.split("\n", trim: true)
@@ -38,8 +21,41 @@ defmodule AdventOfCode201804 do
     end
   end
 
-  def add_guard([], guard_num) do
+  def add_guard([], _guard_num) do
     []
+  end
+
+  def guard_lines_to_ranges(lines) do
+    chunked_lines = lines |> Enum.chunk_every(2)
+    chunked_lines
+      |> Enum.map(fn [{sleep_guard, sleep_text}, {wake_guard, wake_text}] ->
+        sleep_valid = Regex.match?(~r/falls asleep/, sleep_text)
+        wake_valid = Regex.match?(~r/wakes up/, wake_text)
+        if !sleep_valid || !wake_valid || (sleep_guard != wake_guard) do
+          raise "Invalid data"
+        end
+        range = parse_range(sleep_text, wake_text)
+        Map.put(range, :guard_id, sleep_guard)
+      end)
+  end
+
+  def parse_range(sleep_text, wake_text) do
+    sleep_date = parse_date(sleep_text)
+    wake_date = parse_date(wake_text)
+    asleep_mins_count = wake_date.composite_min - sleep_date.composite_min
+    asleep_mins_list = sleep_date.composite_min..(wake_date.composite_min-1) |> Enum.to_list
+    %{asleep_mins_count: asleep_mins_count,
+      asleep_mins_list: asleep_mins_list,
+      sleep_date: sleep_date,
+      wake_date: wake_date}
+  end
+
+  def parse_date(line) do
+    [_year, _month, day, hour, min] = Regex.run(~r/\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\]/, line)
+                              |> List.delete_at(0)
+                              |> Enum.map(fn x -> String.to_integer(x) end)
+    #%{year: year, month: month, day: day, hour: hour, min: min, composite_min: 60 * hour + min}
+    %{day: day, hour: hour, min: min, composite_min: 60 * hour + min}
   end
 
   # line containing guard number -> guard number
@@ -52,12 +68,43 @@ defmodule AdventOfCode201804 do
   #g = get_guard(line)
   #IO.inspect g
 
+  def ranges_to_sleep_time_by_guard(ranges) do
+    ranges
+    |> Enum.reduce(%{}, fn range, acc ->
+      Map.put(acc, range.guard_id, (acc[range.guard_id] || 0) + range.asleep_mins_count)
+    end)
+  end
+
   def go do
     lines = read_file()
       |> Enum.sort()
       |> add_guard(-1)
 
-    IO.puts "[Part 1]: lines"
-    IO.inspect lines
+    ranges = lines
+      |> guard_lines_to_ranges()
+    #IO.inspect ranges, char_lists: :as_lists
+
+    most_sleepy_guard = ranges
+      |> ranges_to_sleep_time_by_guard()
+      |> Map.to_list()
+      |> Enum.max_by(fn {_guard_id, sleep_time} -> sleep_time end)
+      |> elem(0)
+
+    most_sleepy_guard_most_often_slept_minute = ranges
+      |> Enum.filter(fn x -> x.guard_id == most_sleepy_guard end)
+      |> Enum.map(fn x -> x.asleep_mins_list end)
+      |> List.flatten()
+      |> Enum.group_by(fn(x) -> x end)
+      |> Enum.reduce(%{}, fn({k, v}, acc) -> Map.put(acc, k, Enum.count(v)) end)
+      |> Map.to_list()
+      |> Enum.max_by(fn {_sleep_minute, occurances} -> occurances end)
+      |> elem(0)
+
+
+    IO.puts "[Part 1, most sleepy guard and most slept minute]:"
+    IO.puts "Most sleepy guard: "
+    IO.puts most_sleepy_guard
+    IO.puts "Minute most often slept for that guard: "
+    IO.puts most_sleepy_guard_most_often_slept_minute
   end
 end
