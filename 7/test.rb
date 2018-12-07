a8397bc8984ee609bc624b6899a8436bdc7e3d34
Matthew Reishus <mreishus@users.noreply.github.com>
2018-12-07 05:42:40 +0000

    Day 7 part 2 in ruby

--- 7/test.rb
+++ 7/test.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 FILENAME = 'input.txt'
+EXTRA_TIME = 60
 #FILENAME = 'input_small.txt'
+#EXTRA_TIME = 0
 
 def do_next(deps)
   cands = []
@@ -9,7 +11,8 @@ def do_next(deps)
       cands.push(key)
     end
   end
-  cands.sort.first
+  ##pp cands
+  cands.sort
   #pp '-----'
   #z = deps.sort_by {|key, value| value.length}
   ##zz = z.filter{|x| x[1].length == 0}
@@ -17,6 +20,10 @@ def do_next(deps)
   #pp z
   #pp '-----'
   #deps.sort_by {|key, value| value.length}.first.first
+end
+
+def time_to_do(letter)
+  letter.ord - 64 + EXTRA_TIME
 end
 
 require 'pp'
@@ -34,24 +41,84 @@ File.readlines(FILENAME).each do |line|
   deps[nex].push(fir)
 end
 
+
+# Init Workers
+workers = []
+1.upto(5) do
+  worker = {working_on: nil, done_on_second: nil}
+  workers.push(worker)
+end
+
 order = []
 pp deps
 pp deps.keys.length
+
+sec = 0
 while deps.keys.length > 0 do
-  next_step = do_next(deps)
-  order.push(next_step)
+  puts "Second #{sec} started"
+  ## Did anyone finish anything?
+  what_was_finished = []
 
-	deps.each do |key, val|
-    deps[key] = val.reject{|x| x == next_step}
-	end
+  ## Find out what was finished and clear out that worker
+  workers.each_with_index do |x, i| 
+    if x[:done_on_second] == sec && x[:working_on] != nil
+      puts "  -- Clearing out worker #{i}"
+      what_was_finished.push(x[:working_on])
+      order.push(x[:working_on])
+      x[:working_on] = nil
+      x[:done_on_second] = nil
+    end
+  end
 
+  ## Delete stuff that was finished
+  what_was_finished.each do |finished|
+    puts "  -- Deleting #{finished}"
+    deps.each do |key, val|
+      deps[key] = val.reject{|x| x == finished}
+    end
+    deps.delete(finished)
+  end
 
-  deps.delete(next_step)
+  ## What can be done next?
+  next_steps = do_next(deps)
+  ## What are people currently working on?
+  working_on = workers.map{|x| x[:working_on]}
+
+  ## If we're working on something remove it from next steps
+  working_on.each{ |x| next_steps.reject! {|y| y == x }}
+
+  ## If next steps has something in it, assign it to the first available worker
+  workers.each_with_index do |x, i| 
+    if x[:working_on] == nil && next_steps.length > 0
+      x[:working_on] = next_steps.shift
+      x[:done_on_second] = sec + time_to_do( x[:working_on] )
+      puts " +++ Assigning #{x[:working_on]} to worker #{i}, will be done on second #{x[:done_on_second]} "
+    end
+  end
+
+  # Increment time
+  sec += 1
 end
 
-pp order
+#while deps.keys.length > 0 do
+#  next_step = do_next(deps)
+#  order.push(next_step)
+#
+#	deps.each do |key, val|
+#    deps[key] = val.reject{|x| x == next_step}
+#	end
+#  deps.delete(next_step)
+#
+#
+#end
+
+puts "Seconds:"
+pp sec
+#pp order
 puts order.join
 
 #a = deps.sort_by {|key, value| value.length}.first.first
 #pp deps
 #pp a
+
+# Guessed 1235, didn't work -- It was 1234 LOL, off by one error
