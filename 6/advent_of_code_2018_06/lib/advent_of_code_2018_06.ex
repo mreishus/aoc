7713574e5aa26c87469ae65c3eed98bf056095e3
Matthew Reishus <mreishus@users.noreply.github.com>
2018-12-08 17:04:04 +0000

    Day 6 part 1 in elixir

--- 6/advent_of_code_2018_06/lib/advent_of_code_2018_06.ex
+++ 6/advent_of_code_2018_06/lib/advent_of_code_2018_06.ex
@@ -23,7 +23,7 @@ defmodule AdventOfCode201806 do
   """
   def find_first_index_in_list(list, needle) do
     matches = list
-      |> Enum.zip(0..length(list)) 
+      |> Enum.zip(0..length(list))
       |> Enum.filter(fn {x, _} -> x == needle end)
     case matches do
       [] -> nil
@@ -71,7 +71,7 @@ defmodule AdventOfCode201806 do
     max_y = coords |> Enum.map(fn x -> elem(x, 1) end) |> Enum.max
     # Adding one space on all dimensions
     # For example, if coords go from 1-5, we have board 0-6
-    {max_x + 1, max_y + 1} 
+    {max_x + 1, max_y + 1}
   end
 
   def generate_board(coords) do
@@ -98,19 +98,56 @@ defmodule AdventOfCode201806 do
     end)
   end
 
-  def go do
-    true
+  def calculate_areas(grid, coords) do
+    areas = 0..(length(coords) - 1)
+      |> Enum.reduce(%{}, fn x, acc ->
+        Map.put(acc, x, %{coord: x, size: 0, is_infinite: false})
+      end)
+
+    {size_x, size_y} = coords_to_board_size(coords)
+
+    0..size_x |> Enum.reduce(areas, fn x, acc ->
+      0..size_y |> Enum.reduce(acc, fn y, acc ->
+        # Add 1 to size of the assigned area
+        assigned_coord = grid[{x, y}].assigned_coord
+        acc = case assigned_coord do
+          -1 -> acc # Assigned = Tie, update no areas
+          _ -> update_in(acc[assigned_coord].size, &(&1 + 1))
+        end
+
+        # Mark as Infinite if needed
+        case grid[{x, y}].is_infinite && assigned_coord != -1 do
+          true -> put_in(acc[assigned_coord].is_infinite, true)
+          false -> acc
+        end
+      end)
+    end)
   end
 
   @doc """
-  iex> AdventOfCode201806.part1()
-  true
+  iex> AdventOfCode201806.part1("input_small.txt")
+  17
   """
   def part1(filename \\ "input_small.txt") do
     coords = ParseFile.filename_to_coords(filename)
     grid = generate_board(coords)
-
-    #IO.inspect(grid)
-    true
+    areas = calculate_areas(grid, coords)
+    # What is the size of the largest area that isn't infinite?
+    areas
+      |> Map.values
+      |> Enum.filter(fn x -> !x.is_infinite end)
+      |> Enum.map(fn x -> x.size end)
+      |> Enum.max
   end
+
+  def part2(filename \\ "input_small.txt") do
+    "WIP"
+  end
+
+  def go do
+    filename = "input.txt"
+    IO.puts AdventOfCode201806.part1(filename)
+    IO.puts AdventOfCode201806.part2(filename)
+  end
+
 end
