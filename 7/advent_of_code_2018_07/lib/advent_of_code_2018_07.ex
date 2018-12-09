0593ab40b5f1e9694033d5015e0aa7fc60fd6734
Matthew Reishus <mreishus@users.noreply.github.com>
2018-12-09 01:29:14 +0000

    Day 7, part 1 in elixir

--- 7/advent_of_code_2018_07/lib/advent_of_code_2018_07.ex
+++ 7/advent_of_code_2018_07/lib/advent_of_code_2018_07.ex
@@ -15,7 +15,7 @@ defmodule AdventOfCode201807 do
   end
 
   def parse_lines(lines) do
-    deps = lines
+    lines
         |> Enum.map(&parse_line/1)
         |> Enum.reduce(%{}, fn [do_first, do_next], acc -> 
           acc = acc
@@ -28,7 +28,7 @@ defmodule AdventOfCode201807 do
 
   def parse_line(line) do
     regex = ~r/Step (\w+) must be finished before step (\w+) can begin./
-    [do_first, do_next] = Regex.run(regex, line) |> List.delete_at(0)
+    [_do_first, _do_next] = Regex.run(regex, line) |> List.delete_at(0)
   end
 
   @doc """
@@ -43,7 +43,7 @@ defmodule AdventOfCode201807 do
   }
   """
   def parse_file(filename) do
-    read_file("input_small.txt")
+    read_file(filename)
       |> parse_lines()
   end
 
@@ -52,9 +52,53 @@ defmodule AdventOfCode201807 do
   true
   """
   def go do
-    deps = parse_file("input_small.txt")
+    deps = parse_file("input.txt")
     IO.inspect deps
+    a = part1(deps)
+    IO.inspect a
     IO.puts "go"
     true
+  end
+
+  def part1(deps) do
+    task_order([], deps) |> Enum.join
+  end
+
+  @doc """
+  iex> AdventOfCode201807.task_order([], %{ "A" => ["C"], "B" => ["A"], "C" => [], "D" => ["A"], "E" => ["F", "D", "B"], "F" => ["C"] })
+  ["C", "A", "B", "D", "F", "E"]
+  """
+  def task_order(result, deps) do
+    next_step = do_next(deps)
+    result = [ next_step | result ]
+
+    new_deps = Map.delete(deps, next_step)
+    new_deps = Map.keys(new_deps)
+    |> Enum.reduce(new_deps, fn x, acc ->
+      vals = acc[x] |> Enum.filter(fn y -> y != next_step end)
+      Map.put(acc, x, vals)
+    end)
+
+    case length(Map.keys(new_deps)) do
+      0 -> result |> Enum.reverse()
+      _ -> task_order(result, new_deps)
+    end
+  end
+
+  @doc """
+  iex> AdventOfCode201807.do_next(%{ "A" => ["C"], "B" => ["A"], "C" => [], "D" => ["A"], "E" => ["F", "D", "B"], "F" => ["C"] })
+  "C"
+  iex> AdventOfCode201807.do_next(%{ "A" => ["C"], "B" => ["A"], "C" => [], "AAA" => [], "D" => ["A"], "E" => ["F", "D", "B"], "F" => ["C"] })
+  "AAA"
+  iex> AdventOfCode201807.do_next(%{ "A" => ["C"], "B" => ["A"], "D" => ["A"], "E" => ["F", "D", "B"], "F" => ["C"] })
+  nil
+  """
+  def do_next(deps) do
+    deps 
+      |> Map.to_list() 
+      |> Enum.filter(fn {_task, deps} -> length(deps) == 0 end) 
+      |> Enum.map(fn x -> elem(x, 0) end) 
+      |> Enum.sort()
+      |> List.first()
   end
 end
