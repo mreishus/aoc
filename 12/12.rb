0c3442fde902ea1e944832c33f74360b946ce549
Matthew Reishus <mreishus@users.noreply.github.com>
2018-12-12 05:45:05 +0000

    day 12

---
+++ 12/12.rb
@@ -0,0 +1,99 @@
+#!/usr/bin/env ruby
+
+require 'pp'
+
+def extend_left(state, xoffset)
+  if state[0] == "#"
+    return [".." + state, xoffset-2]
+  elsif state[1] == "#"
+    return ["." + state, xoffset-1]
+  end
+  [state, xoffset]
+end
+
+def extend_right(state)
+  if state[state.length - 1] == "#"
+    return state + ".."
+  elsif state[state.length - 2] == "#"
+    return state + "."
+  end
+  state
+end
+
+def extend(state, xoffset)
+  extend_left(extend_right(state), xoffset)
+end
+
+def new_state(state, map, xoffset)
+  state, xoffset = extend(state, xoffset)
+  new_state = ''
+
+  range = 0..(state.length-1)
+  range.each do |x|
+    parent_string = ""
+    (-2).upto(2) do |z|
+      look_i = x + z
+      if range.include?(look_i)
+        parent_string += state[look_i]
+      else
+        parent_string += "."
+      end
+    end
+    #puts parent_string
+
+    if map.has_key?(parent_string)
+      new_state += map[parent_string]
+    else
+      new_state += "."
+    end
+
+  end
+
+  [new_state, xoffset]
+end
+
+def getcount(state, xoffset)
+  count = 0
+  state.chars.each_with_index do |c, i|
+    if c == "#"
+      count += (i + xoffset)
+    end
+  end
+  count
+end
+
+FILENAME = 'input.txt'
+#FILENAME = 'input_small.txt'
+
+lines = File.readlines(FILENAME)
+initial_state = lines.shift.strip
+lines.shift # Blank line
+
+initial_state, _ = initial_state.match(/initial state: ([#\.]+)/).captures
+map = {}
+lines.each do |line|
+  left, right = line.strip.match(/([\.#]+) => ([#\.])/).captures
+  map[left] = right
+end
+
+
+state = initial_state
+xoffset = 0
+pp state
+#1.upto(20) do |x|
+last_count = 0
+1.upto(50000000000) do |x|
+  state, xoffset = new_state(state, map, xoffset)
+  if x % 1000 == 0
+    puts x
+    new_count =  getcount(state, xoffset)
+    puts "#{new_count}  diff #{new_count - last_count}"
+    how_many_more = (50000000000-x) / 1000
+    #/ 1000) * (new_count-last_count)
+    puts " hmm... estimate: #{(how_many_more * (new_count - last_count)) + new_count}"
+    last_count = new_count
+  end
+  #pp state
+end
+
+## 9699999999321
