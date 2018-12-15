8fba96c9fbf238efee1030c0720eb5f861216524
Matthew Reishus <mreishus@users.noreply.github.com>
2018-12-15 03:06:53 +0000

    Day 14, part 2 - slow, needs refactor

--- 14/14.rb
+++ 14/14.rb
@@ -48,8 +48,41 @@ def part1(target)
   puts board[target..(target+9)].map(&:to_s).join("")
 end
 
-[9, 5, 18, 2018, 110201].each do |x|
-  puts "Part 1, target: #{x}"
-  part1(x)
+def part2(target)
+  target_string = target.to_s
+  board, e1_select, e2_select = init
+
+  result = nil
+  while (result == nil) do
+    board_add! board, e1_select, e2_select
+    e1_select, e2_select = move_selections(board, e1_select, e2_select)
+
+    board_string = board.map(&:to_s).join("")
+    search = board_string.index(target_string)
+    if search != nil
+      result = search
+    end
+  end
+  puts "answer:"
+  puts result
+end
+
+do_part1 = false
+if do_part1
+  [9, 5, 18, 2018, 110201].each do |x|
+    puts "Part 1, target: #{x}"
+    part1(x)
+    puts ""
+  end
+end
+
+# 51589 first appears after 9 recipes.
+# 01245 first appears after 5 recipes.
+# 92510 first appears after 18 recipes.
+# 59414 first appears after 2018 recipes.
+
+[51589, 01245, 92510, 59414, 110201].each do |x|
+  puts "Part 2, target: #{x}"
+  part2(x)
   puts ""
 end
