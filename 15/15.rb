c680ccf166528e894aac990c9bd9dbe216a416ce
Matthew Reishus <mreishus@users.noreply.github.com>
2018-12-15 19:12:56 +0000

    Day 15, add tick and tick_unit

--- 15/15.rb
+++ 15/15.rb
@@ -16,6 +16,7 @@ def readfile(filename)
   max_x, max_y = readfile_coords(filename)
   grid = Array.new(max_x+1) { Array.new(max_y + 1)}
   units = []
+  unit_id = 0
   y = 0
   File.readlines(filename).each do |line|
     x = 0
@@ -25,11 +26,13 @@ def readfile(filename)
         grid[x][y] = '#'
       elsif c == 'G'
         grid[x][y] = '.'
-        unit = {x: x, y: y, type: 'gob', display: 'G'}
+        unit = {x: x, y: y, type: 'gob', display: 'G', id: unit_id}
+        unit_id += 1
         units.push unit
       elsif c == 'E'
         grid[x][y] = '.'
-        unit = {x: x, y: y, type: 'elf', display: 'E'}
+        unit = {x: x, y: y, type: 'elf', display: 'E', id: unit_id}
+        unit_id += 1
         units.push unit
       elsif c == '.'
         grid[x][y] = '.'
@@ -37,7 +40,7 @@ def readfile(filename)
     end
     y += 1
   end
-  [grid, units, max_x, max_y]
+  {grid: grid, units: units, max_x: max_x, max_y: max_y}
 end
 
 # Gets max_x and max_y, 0 indexed, from file
@@ -54,7 +57,9 @@ def readfile_coords(filename)
   [max_x, max_y]
 end
 
-def display(grid, units, max_x, max_y)
+def display(gamedata)
+  grid, units, max_x, max_y = gamedata.values_at(:grid, :units, :max_x, :max_y)
+
   0.upto(max_y) do |y|
     0.upto(max_x) do |x|
       #puts "x [#{x}]"
@@ -65,9 +70,28 @@ def display(grid, units, max_x, max_y)
   end
 end
 
+def tick(input_gamedata)
+  gamedata = input_gamedata.dup
+  units = gamedata[:units]
+  turn_order = units.sort_by {|h| [ h[:y].to_i, h[:x].to_i ]}.map {|u| u[:id] }
+  turn_order.each do |id| 
+    gamedata = tick_unit(gamedata, id)
+  end
+  gamedata
+end
+
+def tick_unit(input_gamedata, id)
+  gamedata = input_gamedata.dup
+  unit = gamedata[:units].find{ |u| u[:id] == id }
+  unit[:x] += 1
+  gamedata
+end
+
 def part1(filename)
-  grid, units, max_x, max_y = readfile(filename)
-  display(grid, units, max_x, max_y)
+  gamedata = readfile(filename)
+  display(gamedata)
+  gamedata = tick(gamedata)
+  display(gamedata)
 end
 
 begin_tests = Time.now
