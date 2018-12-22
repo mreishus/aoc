b0b0280b670f132b1ae33fc1080ca4369a078650
Matthew Reishus <mreishus@users.noreply.github.com>
2018-12-22 05:46:12 +0000

    Moved main methods inside class

--- 22/22.rb
+++ 22/22.rb
@@ -10,56 +10,59 @@ end
 
 def tests
   depth = 510
-  raise 'ind01' unless geoind( Complex(0, 0), Complex(10, 10), depth ) == 0
-  raise 'ind01' unless geoind( Complex(10, 10), Complex(10, 10), depth ) == 0
+  raise 'ind01' unless Grid.geoind( Complex(0, 0), Complex(10, 10), depth ) == 0
+  raise 'ind01' unless Grid.geoind( Complex(10, 10), Complex(10, 10), depth ) == 0
 
-  raise 'f1' unless geoind( Complex(1, 0), Complex(10, 10), depth ) == 16807
-  raise 'f2' unless erosionlevel( Complex(1, 0), Complex(10, 10), depth ) == 17317
-  raise 'f3' unless type( Complex(1, 0), Complex(10, 10), depth ) == 1
+  raise 'f1' unless Grid.geoind( Complex(1, 0), Complex(10, 10), depth ) == 16807
+  raise 'f2' unless Grid.erosionlevel( Complex(1, 0), Complex(10, 10), depth ) == 17317
+  raise 'f3' unless Grid.type( Complex(1, 0), Complex(10, 10), depth ) == 1
 
-  raise 'f4' unless geoind( Complex(0, 1), Complex(10, 10), depth ) == 48271
-  raise 'f5' unless erosionlevel( Complex(0, 1), Complex(10, 10), depth ) == 8415
-  raise 'f6' unless type( Complex(0, 1), Complex(10, 10), depth ) == 0
+  raise 'f4' unless Grid.geoind( Complex(0, 1), Complex(10, 10), depth ) == 48271
+  raise 'f5' unless Grid.erosionlevel( Complex(0, 1), Complex(10, 10), depth ) == 8415
+  raise 'f6' unless Grid.type( Complex(0, 1), Complex(10, 10), depth ) == 0
 
-  raise 'f7' unless geoind( Complex(1, 1), Complex(10, 10), depth ) == 145722555
-  raise 'f8' unless erosionlevel( Complex(1, 1), Complex(10, 10), depth ) == 1805
-  raise 'f9' unless type( Complex(1, 1), Complex(10, 10), depth ) == 2
+  raise 'f7' unless Grid.geoind( Complex(1, 1), Complex(10, 10), depth ) == 145722555
+  raise 'f8' unless Grid.erosionlevel( Complex(1, 1), Complex(10, 10), depth ) == 1805
+  raise 'f9' unless Grid.type( Complex(1, 1), Complex(10, 10), depth ) == 2
 
-  raise 'f10' unless geoind( Complex(10, 10), Complex(10, 10), depth ) == 0
-  raise 'f11' unless erosionlevel( Complex(10, 10), Complex(10, 10), depth ) == 510
-  raise 'f12' unless type( Complex(10, 10), Complex(10, 10), depth ) == 0
+  raise 'f10' unless Grid.geoind( Complex(10, 10), Complex(10, 10), depth ) == 0
+  raise 'f11' unless Grid.erosionlevel( Complex(10, 10), Complex(10, 10), depth ) == 510
+  raise 'f12' unless Grid.type( Complex(10, 10), Complex(10, 10), depth ) == 0
 
-  raise 'f13' unless total_risk(Complex(10, 10), depth) == 114
+  raise 'f13' unless Grid.total_risk(Complex(10, 10), depth) == 114
 end
 
-def total_risk(targetcoord, depth)
-  tx, ty = [targetcoord.real, targetcoord.imag]
-  risk = 0
-  0.upto(tx) do |x|
-    0.upto(ty) do |y|
-      risk += type( Complex(x, y), targetcoord, depth)
+class Grid
+  def self.total_risk(targetcoord, depth)
+    tx, ty = [targetcoord.real, targetcoord.imag]
+    risk = 0
+    0.upto(tx) do |x|
+      0.upto(ty) do |y|
+        risk += Grid.type( Complex(x, y), targetcoord, depth)
+      end
     end
+    risk
   end
-  risk
+
+  def self.geoind(coord, targetcoord, depth)
+    x, y = [coord.real, coord.imag]
+    tx, ty = [targetcoord.real, targetcoord.imag]
+    return 0 if (x == 0 && y == 0) || (x == tx && y == ty)
+    return x * 16807 if y.zero?
+    return y * 48271 if x.zero?
+
+    Grid.erosionlevel(Complex(x - 1, y), targetcoord, depth) * Grid.erosionlevel(Complex(x, y - 1), targetcoord, depth)
+  end
+
+  def self.erosionlevel(coord, targetcoord, depth)
+    (Grid.geoind(coord, targetcoord, depth) + depth) % 20183
+  end
+
+  def self.type(coord, targetcoord, depth)
+    Grid.erosionlevel(coord, targetcoord, depth) % 3
+  end
 end
 
-def geoind(coord, targetcoord, depth)
-  x, y = [coord.real, coord.imag]
-  tx, ty = [targetcoord.real, targetcoord.imag]
-  return 0 if (x == 0 && y == 0) || (x == tx && y == ty)
-  return x * 16807 if y.zero?
-  return y * 48271 if x.zero?
-
-  erosionlevel(Complex(x - 1, y), targetcoord, depth) * erosionlevel(Complex(x, y - 1), targetcoord, depth)
-end
-
-def erosionlevel(coord, targetcoord, depth)
-  (geoind(coord, targetcoord, depth) + depth) % 20183
-end
-
-def type(coord, targetcoord, depth)
-  erosionlevel(coord, targetcoord, depth) % 3
-end
 
 ############## MAIN #####################
 
