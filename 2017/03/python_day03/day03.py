afbcae0bc4381e1ad198f0e2f66f81bb5f5e09a2
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-26 21:25:18 -0600

    2017/03b python

--- 2017/03/python_day03/day03.py
+++ 2017/03/python_day03/day03.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 
 """
 Advent of Code 2037 Day 03.
@@ -49,7 +50,39 @@ class Day03:
             y += delta_y
         return abs(x) + abs(y)
 
+    def neighbor_sum(grid, x, y):
+        if x == 0 and y == 0:
+            return 1
+        sum = 0
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if dx == 0 and dy == 0:
+                    continue
+                sum += grid[(x + dx, y + dy)]
+        return sum
+
+    def part2(target):
+        grid = defaultdict(lambda: 0)
+        grid[(0, 0)] = 1
+        x = -1
+        y = 0
+        delta_x = 1
+        delta_y = 0
+        while 1:
+            if Day03.should_rotate(x, y):
+                # print("rotating")
+                delta_x, delta_y = delta_y * -1, delta_x
+            x += delta_x
+            y += delta_y
+            neighbor_sum = Day03.neighbor_sum(grid, x, y)
+            grid[(x, y)] = neighbor_sum
+            if neighbor_sum > target:
+                return neighbor_sum
+            # print(f"x{x} y{y} sum{neighbor_sum}")
+
 
 if __name__ == "__main__":
     print("Part1: ")
     print(Day03.part1(265149))
+    print("Part2: ")
+    print(Day03.part2(265149))
