f39e0afdb60fdc022b88b8807aada62b6fd883db
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-01 18:14:37 -0600

    python/black

--- 2019/01/python_day01/day01.py
+++ 2019/01/python_day01/day01.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+
 def fuel(num):
     return num // 3 - 2
+
 
 def total_fuel(f):
     total = 0
@@ -13,11 +15,14 @@ def total_fuel(f):
 
     return total
 
+
 def part1(data):
     return sum([fuel(num) for num in data])
 
+
 def part2(data):
     return sum([total_fuel(num) for num in data])
+
 
 def main():
     data = [int(line.strip()) for line in open("../input.txt").readlines()]
@@ -25,6 +30,7 @@ def main():
     print(part1(data))
     print("Part2:")
     print(part2(data))
+
 
 if __name__ == "__main__":
     main()
