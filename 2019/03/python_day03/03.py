5351aa035f0d9b64862d3abff43e66ddbf65431c
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-03 12:48:18 -0600

    2019/03ab: python refactor

--- 2019/03/python_day03/03.py
+++ 2019/03/python_day03/03.py
@@ -9,14 +9,20 @@ DIRECTIONS = {
 }
 
 
+# filename -> [ wires ]
+# wire example: [ ("U", 5), ("D", 40), ... ]
 def parse(filename):
     return [parse_line(line) for line in open(filename).readlines()]
 
 
+# string -> wire
+# wire example: [ ("U", 5), ("D", 40), ... ]
 def parse_line(line):
     return [parse_step(step) for step in line.strip().split(",")]
 
 
+# string -> step
+# step example: ("R", 10)
 def parse_step(step):
     direction = step[0]
     distance = int(step[1:])
@@ -27,27 +33,25 @@ def manhattan(coord):
     return abs(coord.real) + abs(coord.imag)
 
 
-def solve(lines):
-    grid_line_ids = defaultdict(list)
+def solve(wires):
+    grid_wire_ids = defaultdict(list)
     grid_steps = defaultdict(list)
-    line_id = 0
-    for line in lines:
-        line_id += 1
+    wire_id = 0
+    for wire in wires:
+        wire_id += 1
         steps = 0
         location = complex(0, 0)
-        for (direction, distance) in line:
+        for (direction, distance) in wire:
             for _ in range(distance):
                 location += DIRECTIONS[direction]
                 steps += 1
-                if line_id not in grid_line_ids[location]:
-                    grid_line_ids[location].append(line_id)
+                if wire_id not in grid_wire_ids[location]:
+                    grid_wire_ids[location].append(wire_id)
                     grid_steps[location].append(steps)
 
-    intersections = [
-        coord for coord in grid_line_ids.keys() if len(grid_line_ids[coord]) > 1
-    ]
-    answer1 = int(min([manhattan(coord) for coord in intersections]))
-    answer2 = int(min([sum(grid_steps[coord]) for coord in intersections]))
+    intersections = [coord for coord in grid_wire_ids if len(grid_wire_ids[coord]) > 1]
+    answer1 = int(min(manhattan(coord) for coord in intersections))
+    answer2 = min(sum(grid_steps[coord]) for coord in intersections)
     return (answer1, answer2)
 
 
