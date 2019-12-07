a4e104695750d8b97b403d0203306399801638bb
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-07 16:54:50 -0600

    2019/07: elixir: Solve part 1

--- 2019/07/elixir_day07/lib/elixir_day07.ex
+++ 2019/07/elixir_day07/lib/elixir_day07.ex
@@ -2,7 +2,7 @@ defmodule ElixirDay07 do
   @moduledoc """
   Documentation for ElixirDay07.
   """
-  alias ElixirDay07.Computer
+  alias ElixirDay07.{Computer, ComputerServer}
 
   def parse(filename) do
     File.stream!(filename)
@@ -12,13 +12,37 @@ defmodule ElixirDay07 do
     |> Enum.map(&String.to_integer/1)
   end
 
-  def main do
-    parse("../../05/input.txt")
-    |> Computer.solve([1])
-    |> IO.inspect(label: "day 5 part1")
+  def permutations([]), do: [[]]
 
-    parse("../../05/input.txt")
-    |> Computer.solve([5])
-    |> IO.inspect(label: "day 5 part2")
+  def permutations(list),
+    do: for(elem <- list, rest <- permutations(list -- [elem]), do: [elem | rest])
+
+  def amplify_once_max_seq(program) when is_list(program) do
+    permutations([0, 1, 2, 3, 4])
+    |> Enum.map(fn phase_sequence ->
+      value = amplify_once(program, phase_sequence)
+      {phase_sequence, value}
+    end)
+    |> Enum.max_by(fn {_phase_sequence, value} ->
+      value
+    end)
+  end
+
+  def amplify_once(program, phase_sequence) when is_list(program) and is_list(phase_sequence) do
+    phase_sequence
+    |> Enum.map(fn phase ->
+      {:ok, pid} = ComputerServer.start(program, [phase])
+      pid
+    end)
+    |> Enum.reduce(0, fn pid, acc ->
+      ComputerServer.add_input(pid, acc)
+      ComputerServer.pop_output(pid)
+    end)
+  end
+
+  def main do
+    parse("../input.txt")
+    |> amplify_once_max_seq()
+    |> IO.inspect(label: "day 5 part1")
   end
 end
--- 2019/07/elixir_day07/test/elixir_day07_test.exs
+++ 2019/07/elixir_day07/test/elixir_day07_test.exs
@@ -4,6 +4,97 @@ defmodule ElixirDay07Test do
 
   alias ElixirDay07.{Computer, ComputerServer}
 
+  ## Day 7 Specific Tests
+  test "amplify_once" do
+    prog_a_1 = [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]
+
+    prog_a_2 = [
+      3,
+      23,
+      3,
+      24,
+      1002,
+      24,
+      10,
+      24,
+      1002,
+      23,
+      -1,
+      23,
+      101,
+      5,
+      23,
+      23,
+      1,
+      24,
+      23,
+      23,
+      4,
+      23,
+      99,
+      0,
+      0
+    ]
+
+    prog_a_3 = [
+      3,
+      31,
+      3,
+      32,
+      1002,
+      32,
+      10,
+      32,
+      1001,
+      31,
+      -2,
+      31,
+      1007,
+      31,
+      0,
+      33,
+      1002,
+      33,
+      7,
+      33,
+      1,
+      33,
+      31,
+      31,
+      1,
+      32,
+      31,
+      31,
+      4,
+      31,
+      99,
+      0,
+      0,
+      0
+    ]
+
+    test_cases = [
+      {prog_a_1, [4, 3, 2, 1, 0], 43210},
+      {prog_a_2, [0, 1, 2, 3, 4], 54321},
+      {prog_a_3, [1, 0, 4, 3, 2], 65210}
+    ]
+
+    # Test amplify_once
+    test_cases
+    |> Enum.each(fn {prog, phase_seq, want_val} ->
+      got_val = ElixirDay07.amplify_once(prog, phase_seq)
+      assert got_val == want_val
+    end)
+
+    # Test amplify_once_max_seq
+    test_cases
+    |> Enum.each(fn {prog, want_seq, want_val} ->
+      {got_seq, got_val} = ElixirDay07.amplify_once_max_seq(prog)
+      assert got_seq == want_seq
+      assert got_val == want_val
+    end)
+  end
+
   ## GenServer tests
 
   test "day 5 part1 GenServer (Full Input)" do
