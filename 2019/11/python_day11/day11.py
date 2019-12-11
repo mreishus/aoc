0bd596e655892081756f86386d7d60ea0b641d21
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-11 10:54:00 -0600

    2019/11ab: python: refactor

--- 2019/11/python_day11/day11.py
+++ 2019/11/python_day11/day11.py
@@ -3,13 +3,6 @@
 from itertools import permutations
 from collections import defaultdict
 
-DIRECTIONS = {
-    "R": complex(1, 0),
-    "L": complex(-1, 0),
-    "U": complex(0, 1),
-    "D": complex(0, -1),
-}
-
 # DEBUG = True
 DEBUG = False
 
@@ -237,10 +230,6 @@ def amplify_loop(program_in, phase_sequence):
         i = (i + 1) % 5
 
 
-def part2(program_in):
-    return amplify_loop_max_seq(program_in)
-
-
 def amplify_loop_max_seq(program_in):
     max_val = 0
     max_sequence = []
@@ -253,122 +242,69 @@ def amplify_loop_max_seq(program_in):
     return [max_val, max_sequence]
 
 
+COMPLEX_OF_DIR = {
+    "U": complex(0, -1),
+    "R": complex(1, 0),
+    "D": complex(0, 1),
+    "L": complex(-1, 0),
+}
+
+
 def turn_right(direction):
-    if direction == "U":
-        return "R"
-    if direction == "L":
-        return "U"
-    if direction == "D":
-        return "L"
-    if direction == "R":
-        return "D"
-    raise "asdf"
+    return turn(direction, 1)
 
 
 def turn_left(direction):
-    if direction == "U":
-        return "L"
-    if direction == "L":
-        return "D"
-    if direction == "D":
-        return "R"
-    if direction == "R":
-        return "U"
-    raise "asdf"
+    return turn(direction, -1)
 
-    # cpus = []
-    # for i in range(5):
-    #     cpus.append(Computer(program_in, [phase_sequence[i]]))
 
-    # i = 0
-    # next_input = 0
-    # while True:
-    #     cpus[i].add_input(next_input)
-    #     cpus[i].execute()
-    #     if cpus[i].state == "halted" and i == 4:
-    #         # print("halted")
-    #         return cpus[i].outputs[0]
-    #     next_input = cpus[i].pop_output()
-    #     i = (i + 1) % 5
+def turn(direction, n):
+    directions = list(COMPLEX_OF_DIR.keys())
+    new_index = (directions.index(direction) + n) % len(directions)
+    return directions[new_index]
+
+
+class PainterRobot(object):
+    def __init__(self, program):
+        self.program = program
+
+    def run_and_return_grid(self, *, initial_color=0):
+        cpu = Computer(self.program, [])
+        grid = defaultdict(lambda: 0)
+        location = complex(0, 0)
+        direction = "U"
+        grid[location] = initial_color
+        # Input to program: 0 if over black (.) , 1 if over white (#)
+        # Outputs two values: color to paint 0/black/. 1/white/#, then 0 = turn left, 1 = turn right
+        while True:
+            current_square = grid[location]
+            cpu.add_input(current_square)
+            cpu.execute()
+            if cpu.state == "halted":
+                break
+            paint_color = cpu.pop_output()
+            turn_dir = cpu.pop_output()
+            grid[location] = paint_color
+            if turn_dir == 1:
+                direction = turn_right(direction)
+            elif turn_dir == 0:
+                direction = turn_left(direction)
+            else:
+                raise "Told to turn an invalid direction"
+            location += COMPLEX_OF_DIR[direction]
+        return grid
 
 
 def part1(program_in):
-    cpu = Computer(program_in, [])
-    grid = defaultdict(lambda: 0)
-    location = complex(0, 0)
-    direction = "U"
-    painted_at_least_once = {}
-    while True:
-        current_square = grid[location]
-        cpu.add_input(current_square)
-        # print(
-        #     f"location[{location}] current_square=input[{current_square}] direction[{direction}]"
-        # )
-        cpu.execute()
-        if cpu.state == "halted":
-            break
-        paint_color = cpu.pop_output()
-        turn_dir = cpu.pop_output()
-        # print(f"  output: paint_color[{paint_color}] turn direction[{turn_dir}]")
-        grid[location] = paint_color
-        if turn_dir == 1:
-            direction = turn_right(direction)
-        elif turn_dir == 0:
-            direction = turn_left(direction)
-        else:
-            raise "Dnuno"
-        location += DIRECTIONS[direction]
-        # print("  new dir: [{direction}] location[{location}]")
-    # print("it halted")
-    # print(grid)
-    # print(list(grid.values()).count(1))
-    # (You guessed 1398.)
-    print(len(list(grid.keys())))
-    # Correct answer (You guessed 2539.)
-    # Input to program: 0 if over black (.) , 1 if over white (#)
-    # Outputs two values: color to paint 0/black/. 1/white/#, then 0 = turn left, 1 = turn right
-    print("Dunno")
+    robot = PainterRobot(program_in)
+    grid = robot.run_and_return_grid(initial_color=0)
+    return len(list(grid.keys()))
 
 
 def part2(program_in):
-    cpu = Computer(program_in, [])
-    grid = defaultdict(lambda: 0)
-    location = complex(0, 0)
-    direction = "U"
-    painted_at_least_once = {}
-    grid[location] = 1
-    while True:
-        current_square = grid[location]
-        cpu.add_input(current_square)
-        # print(
-        #     f"location[{location}] current_square=input[{current_square}] direction[{direction}]"
-        # )
-        cpu.execute()
-        if cpu.state == "halted":
-            break
-        paint_color = cpu.pop_output()
-        turn_dir = cpu.pop_output()
-        # print(f"  output: paint_color[{paint_color}] turn direction[{turn_dir}]")
-        grid[location] = paint_color
-        if turn_dir == 1:
-            direction = turn_right(direction)
-        elif turn_dir == 0:
-            direction = turn_left(direction)
-        else:
-            raise "Dnuno"
-        location += DIRECTIONS[direction]
-        # print("  new dir: [{direction}] location[{location}]")
-    print("it halted")
-    print(grid)
-    # print(list(grid.values()).count(1))
-    # (You guessed 1398.)
-    print(len(list(grid.keys())))
-    # Correct answer (You guessed 2539.)
-    # Input to program: 0 if over black (.) , 1 if over white (#)
-    # Outputs two values: color to paint 0/black/. 1/white/#, then 0 = turn left, 1 = turn right
-    print("Dunno")
-
-    for y in range(-5, 10):
+    robot = PainterRobot(program_in)
+    grid = robot.run_and_return_grid(initial_color=1)
+    for y in range(-3, 7):
         for x in range(-5, 45):
             value = grid[complex(x, y)]
             if value == 1:
@@ -376,15 +312,11 @@ def part2(program_in):
             else:
                 print(" ", end="")
         print("")
+    return "Look above ^"
 
 
 if __name__ == "__main__":
     program = parse("../../11/input.txt")
-    # part1(program)
-    part2(program)
-    # print("Part 1:")
-    # outputs = solve1(file_data, [1])
-    # print(outputs)
-    # print("Part 2:")
-    # outputs = solve1(file_data, [2])
-    # print(outputs)
+    print(part1(program))
+    print("Part 2:")
+    print(part2(program))
