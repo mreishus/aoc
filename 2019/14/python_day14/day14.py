9ab2e98b96a38d7d56097a0a24a93e6d88860b28
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-14 11:27:55 -0600

    2019/14: python: refactor
    I changed the main `what_i_need` list into an `inventory` dictionary.  I
    was going through some serious contortions to loop through the list
    while recording changes to make to it later.. this is much cleaner,
    removes about 60 lines.

--- 2019/14/python_day14/day14.py
+++ 2019/14/python_day14/day14.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
-from collections import Counter
-from itertools import groupby
-import collections
+from collections import namedtuple, defaultdict
+
+Element = namedtuple("Element", "thing quantity")
+Rule = namedtuple("Rule", "materials result")
 
 
 def parse(filename):
     with open(filename) as f:
         return [parse_line(line.strip()) for line in f.readlines()]
-
-
-Element = collections.namedtuple("Element", "thing quantity")
-Rule = collections.namedtuple("Rule", "materials result")
 
 
 def parse_line(line):
@@ -47,13 +44,7 @@ def approx_match(d, elm):
     print(
         f"WHAT?? keys[{keys}] smaller[{smaller}] larger[{larger}] elmQ[{elm.quantity}]"
     )
-
-
-def index_of_first(lst, pred):
-    for i, v in enumerate(lst):
-        if pred(v):
-            return i
-    return None
+    raise ValueError("Approx_match: The world is wrong")
 
 
 def part2(rules):
@@ -65,118 +56,68 @@ def part2(rules):
 
 
 def part1(rules, initial_fuel=1):
-    d = {}
-    for rule in rules:
-        if rule.result in d:
-            raise ValueError("didn't expect to see duplicate rules")
-        d[rule.result] = rule.materials
+    d = {rule.result: rule.materials for rule in rules}
+    inventory = defaultdict(int, {"FUEL": initial_fuel})
+    negatives = defaultdict(int)
 
-    what_i_need = [Element(thing="FUEL", quantity=initial_fuel)]
-    negatives = []
     while True:
-        additions = []
-        delete_indexes = []
+        for (elem_thing, elem_quantity) in list(inventory.items()):
+            if elem_quantity < 0:
+                raise ValueError("Shouldn't happen")
+            elif elem_quantity == 0:
+                del inventory[elem_thing]
+                continue
 
-        for i, elm in enumerate(what_i_need):
-            # Is there a negative thing (EXTRA)?
-            negative_i = index_of_first(negatives, lambda v: v.thing == elm.thing)
-            # print(f"neg #{negative_i} #{negatives}  <==> {elm}")
-
-            # Look for exact replacement
-            if elm in d:
-                # print(f"Using direct replacement.")
-                delete_indexes.append(i)
-                additions += d[elm]
-            elif negative_i != None:
-                # Pos: 2 Neg: -3  Result = -1
-
-                # Mark both for deletion
-                neg = negatives.pop(negative_i)
-                delete_indexes.append(i)
-
-                # Find new amount
-                new_amount = neg.quantity + elm.quantity
-                new_elm = Element(elm.thing, new_amount)
-                if new_amount > 0:
-                    additions += [new_elm]
-                elif new_amount < 0:
-                    negatives.append(new_elm)
+            elm = Element(elem_thing, elem_quantity)
+            if elem_thing in negatives:
+                # Use leftovers
+                new_amount = elem_quantity + negatives[elem_thing]
+                if new_amount >= 0:
+                    inventory[elem_thing] = new_amount
+                    del negatives[elem_thing]
+                else:
+                    del inventory[elem_thing]
+                    negatives[elem_thing] = new_amount
+            elif elm in d:
+                # Direct replacement
+                for (new_thing, new_amount) in d[elm]:
+                    inventory[new_thing] += new_amount
+                del inventory[elem_thing]
             elif approx_match(d, elm):
                 # We have an approx match; use_this = 10 A, produces = 10 ORE, but
                 # we have say, 14 A (or 8 A!)
                 use_this = approx_match(d, elm)
                 produces = d[use_this]
 
-                # print(f"Maybe continue? Got this rule:")
                 # print(f"  --USE THIS --> [{use_this}] ")
                 # print(f"   <--- GET THIS [{produces}]")
 
                 if elm.quantity >= use_this.quantity:
-                    # print(f"     ++ This one is safe. Doing it!")
                     # This one is always safe; invoking a rule to transform 10A when we have 14.
-                    # times = 1
-                    # if elm.quantity % use_this.quantity == 0:
                     times = elm.quantity // use_this.quantity
-
-                    delete_indexes.append(i)
-                    new_elm = Element(
-                        elm.thing, elm.quantity - (use_this.quantity * times)
-                    )
-                    if new_elm.quantity > 0:
-                        additions += [new_elm]
-                    for this_elm in produces:
-                        additions += [
-                            Element(this_elm.thing, this_elm.quantity * times)
-                        ]
+                    inventory[elem_thing] -= use_this.quantity * times
+                    for produced_elm in produces:
+                        inventory[produced_elm.thing] += produced_elm.quantity * times
                     break
                 else:
                     # Actually, not safe/possible, we need to get more, but we will waste some
                     # We only need 8 A but the rule is 10 ORE => 10 A
-                    delete_indexes.append(i)
-                    additions += produces
-                    negatives.append(
-                        Element(elm.thing, elm.quantity - use_this.quantity)
-                    )
-                    # print("")
-                    # print(what_i_need)
-                    # print(f"  --USE THIS --> [{use_this}] ")
-                    # print(f"   <--- GET THIS [{produces}]")
+                    new_amount = elm.quantity - use_this.quantity
+                    del inventory[elem_thing]
+                    negatives[elem_thing] += new_amount
+
+                    for produced_elm in produces:
+                        inventory[produced_elm.thing] += produced_elm.quantity
                     # print(f"     -- Hmm.. producing waste.")
                     break
-                # else:
-                # print(f"     ?? Decided to skip for now.")
 
-        # Process Marked Deletes/Adds
-        for i in sorted(delete_indexes, reverse=True):
-            what_i_need.pop(i)
-        what_i_need += additions
-
-        # Merge Same Types
-        merged = []
-        for thing, elements in groupby(sorted(what_i_need), key=lambda x: x.thing):
-            q = sum(x.quantity for x in elements)
-            merged.append(Element(thing=thing, quantity=q))
-        what_i_need = merged
-
-        # print("")
-        # print("What I need:")
-        # print(what_i_need)
-        # print("")
-
-        if len(what_i_need) == 1 and what_i_need[0].thing == "ORE":
+        if len([k for k, v in inventory.items() if v > 0]) == 1:
             break
-
-        # j += 1
-        # if j > 10000000:
-        #     break
-    # print("What I need:")
-    # print(what_i_need)
-    # print("Negatives:")
-    # print(negatives)
-    return what_i_need[0].quantity
+    return inventory["ORE"]
 
 
 if __name__ == "__main__":
+    # rules = parse("../input_p1_165.txt")
     rules = parse("../input.txt")
     print("Part1: ")
     print(part1(rules))
