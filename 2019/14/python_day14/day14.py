0f8e93d409aa191ec5568368edeb1243f223740f
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-14 11:35:29 -0600

    2019/14: python: refactor

--- 2019/14/python_day14/day14.py
+++ 2019/14/python_day14/day14.py
@@ -31,7 +31,6 @@ def approx_match(d, elm):
     # But there is no direct key in d corresponding to "28 A"
     # Find another one like "10 A"
     keys = [k for k, v in d.items() if k.thing == elm.thing]
-    # print(f"Found [{keys}]")
     if len(keys) == 0:
         return None
 
@@ -42,7 +41,7 @@ def approx_match(d, elm):
     if len(larger) > 0:
         return max(larger, key=lambda x: x.quantity)
     print(
-        f"WHAT?? keys[{keys}] smaller[{smaller}] larger[{larger}] elmQ[{elm.quantity}]"
+        f"Can't find a match keys[{keys}] smaller[{smaller}] larger[{larger}] elmQ[{elm.quantity}]"
     )
     raise ValueError("Approx_match: The world is wrong")
 
@@ -64,7 +63,7 @@ def part1(rules, initial_fuel=1):
         for (elem_thing, elem_quantity) in list(inventory.items()):
             if elem_quantity < 0:
                 raise ValueError("Shouldn't happen")
-            elif elem_quantity == 0:
+            if elem_quantity == 0:
                 del inventory[elem_thing]
                 continue
 
@@ -92,24 +91,20 @@ def part1(rules, initial_fuel=1):
                 # print(f"  --USE THIS --> [{use_this}] ")
                 # print(f"   <--- GET THIS [{produces}]")
 
-                if elm.quantity >= use_this.quantity:
-                    # This one is always safe; invoking a rule to transform 10A when we have 14.
-                    times = elm.quantity // use_this.quantity
-                    inventory[elem_thing] -= use_this.quantity * times
-                    for produced_elm in produces:
-                        inventory[produced_elm.thing] += produced_elm.quantity * times
-                    break
+                # Add the new stuff we made
+                times = max(elm.quantity // use_this.quantity, 1)
+                for produced_elm in produces:
+                    inventory[produced_elm.thing] += produced_elm.quantity * times
+
+                # Take away what we used
+                new_amount = elm.quantity - use_this.quantity * times
+                if new_amount >= 0:
+                    inventory[elem_thing] = new_amount
                 else:
-                    # Actually, not safe/possible, we need to get more, but we will waste some
-                    # We only need 8 A but the rule is 10 ORE => 10 A
-                    new_amount = elm.quantity - use_this.quantity
+                    # print(f"     -- Hmm.. producing waste.")
                     del inventory[elem_thing]
                     negatives[elem_thing] += new_amount
-
-                    for produced_elm in produces:
-                        inventory[produced_elm.thing] += produced_elm.quantity
-                    # print(f"     -- Hmm.. producing waste.")
-                    break
+                break
 
         if len([k for k, v in inventory.items() if v > 0]) == 1:
             break
