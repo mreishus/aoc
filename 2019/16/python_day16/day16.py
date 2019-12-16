b3f9796fee2c95a1a6c4ac1b92d842a62a3c8834
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-16 11:05:27 -0600

    2019/16: python: cleanup

--- 2019/16/python_day16/day16.py
+++ 2019/16/python_day16/day16.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python
 
-## Note: Day 2 doesn't work yet
-
 import itertools
 from collections import deque
-
-# import numpy as np
-
-# np.set_printoptions(edgeitems=10)
-# np.core.arrayprint._line_width = 180
-
-# from aoc.computer import Computer
-# from aoc.day15 import Day15
 
 
 def parse(filename):
@@ -40,32 +30,13 @@ def sequence(n):
 def transform(digits):
     result = []
     for output_i, _ in enumerate(digits):
-        # if output_i % 10 == 0:
-        #     print(f"  {output_i}", end="")
-
-        # Skip 1 - Old school
-        # seq = sequence(output_i + 1)
-        # next(seq)
-
-        # Skip 1 - New
-        # seq = itertools.islice(sequence(output_i + 1), 1, None)
-
         # Skip N + 1 | Combine with slicing "for digits in digits[output:i]" to skip
         # half the triangle
         seq = itertools.islice(sequence(output_i + 1), 1 + output_i, None)
 
         this_digit = 0
-        # for digit in itertools.islice(digits, output_i, None):
-        # for digit in digits[output_i:]:
-        for i in range(output_i, len(digits)):
-            # print(digit, end="")
-            # this_digit += next(seq) * digit
-            # a = next(seq)
-            # # print(f"{a} * {digit}   +   ", end="")
-            # print(f"{a} * {digit}   +   ")
-            this_digit += next(seq) * digits[i]
-            # print(f"            {this_digit}")
-        # print(f" = {this_digit}")
+        for digit in itertools.islice(digits, output_i, None):
+            this_digit += next(seq) * digit
         this_digit = abs(this_digit) % 10
         result.append(this_digit)
     return result
@@ -73,7 +44,6 @@ def transform(digits):
 
 def multiple_transform(digits, n):
     for i in range(n):
-        print(i)
         digits = transform(digits)
     return digits
 
@@ -85,12 +55,6 @@ def transform_secondhalf(digits):
         num = (num + digits.pop()) % 10
         result.appendleft(num)
     return result
-    # result = deque()
-    # num = 0
-    # for i in range(len(digits) - 1, -1, -1):
-    #     num = (num + digits[i]) % 10
-    #     result.appendleft(num)
-    # return result
 
 
 def part1_secondhalf(digits):
@@ -105,9 +69,7 @@ def part1_secondhalf(digits):
 
 def part1(digits):
     result = multiple_transform(digits, 100)
-    return result
-    # print(result)
-    # return "".join(str(x) for x in result[:8])
+    return "".join(str(x) for x in result[:8])
 
 
 def part2(digits):
@@ -119,51 +81,21 @@ def part2(digits):
     second_half = deque(digits * 5000)
     print("Starting to iterate")
     for i in range(100):
-        # for i in range(3):
-        print(f"Step {i}")
         second_half = transform_secondhalf(second_half)
 
-    # result = multiple_transform(digits, 100)
     result = list(second_half)
     skip = message_offset - (len(digits) * 5000)
-    print(f"Skip [{skip}]")
-    print(f"Result length [{len(result)}]")
+    # print(f"Skip [{skip}]")
+    # print(f"Result length [{len(result)}]")
     answer = result[skip : 8 + skip]
     print(f"Found answer {answer} ...")
-    return answer
+    return int("".join(str(x) for x in answer))
 
 
 if __name__ == "__main__":
-
-    # digits = parse16("../../16/input_small.txt")
-    # print(digits)
-    # for i in range(4):
-    #     print(digits)
-    #     digits = transform(digits)
-    # digits = parse16_string("80871224585914546619083218645595")
-
-    print("---")
     digits = parse16("../../16/input.txt")
 
-    print("P1 end: Look for 985635")
-    # print(part1(digits))
-    # print(part1_secondhalf(digits))
+    print("Part 1:")
+    print(part1(digits))
+    print("Part 2:")
     print(part2(digits))
-
-    # inn = parse16("../../16/input.txt")
-    # digits = parse16_string("80871224585914546619083218645595")
-    # print(multiple_transform(digits, 100))
-    # print("Part 1:")
-
-    # it1 = sequence(1)
-    # it2 = sequence(2)
-    # print("Seq 1")
-    # for i in range(20):
-    #     print(next(it1))
-    # print("Seq 2")
-    # for i in range(20):
-    #     print(next(it2))
-
-    # for phase in range(1, 10):
-    #     print(f"Phase {phase}")
-    # # print(Day15.part1(program))
