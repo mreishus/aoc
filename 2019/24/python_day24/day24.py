6861bf359345965cb4efa6b5b8d8a56ddbcaee60
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-24 01:31:40 -0600

    2019/24: python part 2, rough
    destroyed the ability to do part 1 as well

--- 2019/24/python_day24/day24.py
+++ 2019/24/python_day24/day24.py
@@ -6,11 +6,15 @@ from collections import defaultdict, deque, Counter
 # from aoc.day23 import Day23
 
 
+layer_min = -150
+layer_max = 150
+
+
 def grid_get_reals_imags(grid):
     """ Given a grid (dict with complex keys), get a list of all of its real
     (x) and imaginary (y) components """
-    reals = [c.real for c in grid.keys()]
-    imags = [c.imag for c in grid.keys()]
+    reals = [c.real for c, z in grid.keys()]
+    imags = [c.imag for c, z in grid.keys()]
     return reals, imags
 
 
@@ -23,14 +27,88 @@ def generate_coords(grid):
             yield x, y
 
 
-def generate_neighbors(coord):
+def ok_z(z):
+    return layer_min <= z < layer_max
+
+
+def generate_neighbors(coord, z):
     """ Given a coordinate (complex), return a generator iterating over its 4 direct neighbors. """
     x = int(coord.real)
     y = int(coord.imag)
-    yield complex(x, y - 1)
-    yield complex(x, y + 1)
-    yield complex(x + 1, y)
-    yield complex(x - 1, y)
+    if x == 2 and y == 1:
+        # Top row, middle column
+        # Yield all but bottom
+        yield complex(x, y - 1), z
+        yield complex(x + 1, y), z
+        yield complex(x - 1, y), z
+        # Special - Yield top edge of inner
+        if ok_z(z + 1):
+            yield complex(0, 0), z + 1
+            yield complex(1, 0), z + 1
+            yield complex(2, 0), z + 1
+            yield complex(3, 0), z + 1
+            yield complex(4, 0), z + 1
+        pass
+    elif x == 3 and y == 2:
+        # Middle row, right column
+        # Yield all but left
+        yield complex(x, y - 1), z
+        yield complex(x, y + 1), z
+        yield complex(x + 1, y), z
+        # Special - Right edge of inner
+        if ok_z(z + 1):
+            yield complex(4, 0), z + 1
+            yield complex(4, 1), z + 1
+            yield complex(4, 2), z + 1
+            yield complex(4, 3), z + 1
+            yield complex(4, 4), z + 1
+        pass
+    elif x == 1 and y == 2:
+        # Middle row, left column
+        # Yield all but right
+        yield complex(x, y - 1), z
+        yield complex(x, y + 1), z
+        yield complex(x - 1, y), z
+        # Special - Left edge of inner
+        if ok_z(z + 1):
+            yield complex(0, 0), z + 1
+            yield complex(0, 1), z + 1
+            yield complex(0, 2), z + 1
+            yield complex(0, 3), z + 1
+            yield complex(0, 4), z + 1
+        pass
+    elif x == 2 and y == 3:
+        # bottom row, middle column
+        # Yield all but top
+        yield complex(x, y + 1), z
+        yield complex(x + 1, y), z
+        yield complex(x - 1, y), z
+        # Special - Yield bottom edge of innter
+        if ok_z(z + 1):
+            yield complex(0, 4), z + 1
+            yield complex(1, 4), z + 1
+            yield complex(2, 4), z + 1
+            yield complex(3, 4), z + 1
+            yield complex(4, 4), z + 1
+        pass
+    else:
+        yield complex(x, y - 1), z
+        yield complex(x, y + 1), z
+        yield complex(x + 1, y), z
+        yield complex(x - 1, y), z
+        if ok_z(z - 1):
+            if y == 0:
+                # Special - "8" of outer
+                yield complex(2, 1), z - 1
+            if y == 4:
+                # special - "18" of outer
+                yield complex(2, 3), z - 1
+            if x == 0:
+                # Special "12" of outer
+                yield complex(1, 2), z - 1
+            if x == 4:
+                # Special "14" of outer
+                yield complex(3, 2), z - 1
 
 
 class Day24:
@@ -39,46 +117,66 @@ class Day24:
         self.parse(filename)
 
     def parse(self, filename):
-        grid = {}
-        # loc_of_door = {}
-        # loc_of_key = {}
+        grid = defaultdict(lambda: ".")
+        # grid = {}
         location = complex(0, 0)
 
         with open(filename) as f:
             for line in f:
                 for char in line.strip():
-                    grid[location] = char
+                    # for z in range(layer_min, layer_max):
+                    grid[location, 0] = char
                     location += complex(1, 0)
                 location += complex(0, 1)
                 location = complex(0, location.imag)
         self.grid = grid
 
     def step(self):
-        new_grid = {}
+        new_grid = defaultdict(lambda: ".")
         for x, y in generate_coords(self.grid):
-            # print(f"{x},{y}")
-            char = self.grid[complex(x, y)]
-            new_char = char
-            adj_bugs = 0
-            for neighbor in generate_neighbors(complex(x, y)):
-                if neighbor in self.grid and self.grid[neighbor] == "#":
-                    adj_bugs += 1
-            if char == "#":
-                if adj_bugs != 1:
-                    new_char = "."
-            elif char == ".":
-                if adj_bugs == 1 or adj_bugs == 2:
-                    new_char = "#"
-            new_grid[complex(x, y)] = new_char
+            is_center = x == 2 and y == 2
+            for z in range(layer_min, layer_max):
+                # print(f"{x},{y}")
+                char = self.grid[complex(x, y), z]
+                if is_center:
+                    char = "?"
+                new_char = char
+                adj_bugs = 0
+                # print(f"{x},{y},{z}")
+                for neighbor in generate_neighbors(complex(x, y), z):
+                    c_neigh, neigh_z = neighbor
+                    neigh_x = int(c_neigh.real)
+                    neigh_y = int(c_neigh.imag)
+                    n_is_center = neigh_x == 2 and neigh_y == 2
+                    if neighbor in self.grid and not n_is_center:
+                        # print(f" --> {neighbor} {neigh_x} {neigh_y} {neigh_z}")
+                        if neighbor in self.grid and self.grid[neighbor] == "#":
+                            adj_bugs += 1
+                if char == "#":
+                    if adj_bugs != 1:
+                        new_char = "."
+                elif char == ".":
+                    if adj_bugs == 1 or adj_bugs == 2:
+                        new_char = "#"
+                new_grid[complex(x, y), z] = new_char
         self.grid = new_grid
 
-    def display(self):
+    def display(self, z):
         reals, imags = grid_get_reals_imags(self.grid)
         for y in range(int(min(imags)) - 0, int(max(imags)) + 1):
             for x in range(int(min(reals)) - 0, int(max(reals)) + 1):
-                char = self.grid[complex(x, y)]
+                char = self.grid[complex(x, y), z]
                 print(char, end="")
             print("")
+
+    def num_bugs(self):
+        count = 0
+        for x, y in generate_coords(self.grid):
+            for z in range(layer_min, layer_max):
+                location = (complex(x, y), z)
+                if location in self.grid and self.grid[location] == "#":
+                    count += 1
+        return count
 
     def score(self):
         n = 1
@@ -101,4 +199,25 @@ class Day24:
 
 if __name__ == "__main__":
     d24 = Day24("../../24/input.txt")
-    print(d24.loop_till_dupe())
+    # d24 = Day24("../../24/input_small.txt")
+    # print(d24.grid)
+    print(d24.num_bugs())
+    for i in range(200):
+        d24.step()
+    print("How many bugs present:")
+    print(d24.num_bugs())
+    print("--")
+    d24.display(0)
+    print("--")
+    d24.display(1)
+    print("--")
+    d24.display(2)
+    print("--")
+    d24.display(3)
+    # print(d24.loop_till_dupe())
+    points = [(complex(3, 3), 0), (complex(0, 0), 0)]
+    for point in points:
+        print(f"Neighbors for #{point}")
+        cx, z = point
+        for n in generate_neighbors(cx, z):
+            print(f"  {n}")
