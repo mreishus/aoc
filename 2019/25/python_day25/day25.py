a2f0cdf962c85927446bbec6e322ea7feba46d09
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-25 13:34:36 -0600

    2019/25: python: add automatic solver

--- 2019/25/python_day25/day25.py
+++ 2019/25/python_day25/day25.py
@@ -3,6 +3,8 @@
 from collections import defaultdict, deque, Counter
 from aoc.computer import Computer
 from aoc.day23 import Day23
+from itertools import combinations
+import networkx as nx
 import copy
 import re
 
@@ -11,7 +13,6 @@ class Day25:
     def __init__(self, program):
         self.program = program
         self.cpu = Computer(self.program, [])
-        self.location = complex(0, 0)
 
     def get_state(self):
         return copy.copy(self.cpu.memory)
@@ -38,91 +39,177 @@ class Day25:
         for i in nums:
             self.cpu.add_input(i)
 
+    def opposite_dir(self, direction):
+        opposites = {"west": "east", "east": "west", "south": "north", "north": "south"}
+        if direction in opposites:
+            return opposites[direction]
+        raise ValueError(f"Don't know opposite of [{direction}]")
+
     def explore(self):
+        self.cpu = Computer(self.program, [])
+        self.loc = None
+        # visited[ 'Hull Breach'] = true
+        # dir_from_to[ ('Hull Breach', 'Holodeck') ] = 'north'
+        # Possibly Delete: dirs_for_loc['Hull Breach'] = ['north', 'west', 'south']
+        # state_for_loc['Hull Breach'] = (memory state for int comp)
+        # loc_of_item['tambourine'] = 'Holodeck'
+        # G = (networkx graph)
         self.visited = {}
-        self.visited[complex(-3, 1)] = True
-        self.path_to = {}
-        self.state_for_coord = {}
-        self.location_of_item = {}
-        self.source = self.location  # (0, 0)
+        self.dir_from_to = {}
+        self.dirs_for_loc = {}
+        self.state_for_loc = {}
+        self.loc_of_item = {}
+        self.G = nx.Graph()
 
-        # Debug: Move
-        # self.execute_silent()
-        # self.send_msg("north")
+        self.explore_dfs(None, None)
 
-        self.explore_dfs(self.source)
-
-        print(self.path_to)
-        print(self.location_of_item)
-
-    def explore_dfs(self, coord):
-        """ This is broken because the game doesn't follow a grid;
-        the arcade overlaps with the Holodeck.  Should change
-        coordintes/paths to use room names instead of coordinates """
-        if coord != self.location:
-            raise ValueError("What")
-
-        # Execute / Print message to screen
+    def explore_dfs(self, command_used, came_from):
+        # Execute, read text and get room name
         message = self.execute_str()
-        print(f"===ExploreDFS [{self.location}]")
-        print(message)
+        room_name = self.parse_title(message)
+        loc = room_name
+        self.loc = loc
+        print(f"=== ExploreDFS [{loc}] ===")
+
+        # Save the way we got here
+        if command_used is not None and came_from is not None:
+            self.G.add_edge(came_from, loc)
+            self.dir_from_to[(came_from, loc)] = command_used
+            self.dir_from_to[(loc, came_from)] = self.opposite_dir(command_used)
+
+        # Been here before?
+        if loc in self.visited:
+            return
 
         # Mark as visited and save state
-        self.visited[coord] = True
-        self.state_for_coord[coord] = self.get_state()
+        self.visited[loc] = True
+        self.state_for_loc[loc] = self.get_state()
 
         # Record items here
-        items = self.parse_items(message)
-        for item in items:
-            self.location_of_item[item] = self.location
+        for item in self.parse_items(message):
+            self.loc_of_item[item] = loc
 
         directions = self.parse_directions(message)
-        for (command, delta) in directions:
-            # Have we been there before?
-            new_coord = coord + delta
-            if new_coord in self.visited:
-                continue
+        self.dirs_for_loc[loc] = directions
 
+        for command in directions:
             # Load state
-            self.set_state(self.state_for_coord[coord])
-            self.location = coord
+            self.set_state(self.state_for_loc[loc])
 
-            # Mark path, Move there and recurse
-            self.path_to[new_coord] = coord
-
-            # (move)
+            # Move and recurse
             self.send_msg(command)
-            self.location += delta
+            self.explore_dfs(command_used=command, came_from=loc)
 
-            self.explore_dfs(new_coord)
+    def pick_up_items(self):
+        print("=== Picking up all items")
+        # Reset computer
+        self.cpu = Computer(self.program, [])
+        message = self.execute_str()
+        loc = self.parse_title(message)
 
-    def move(self, command, delta):
-        pass
+        for item in self.loc_of_item:
+            if self.is_blacklisted(item):
+                continue
+            destination = self.loc_of_item[item]
+            self.move(loc, destination)
+            loc = destination
+            self.loc = loc
+            self.send_msg(f"take {item}")
+            message = self.execute_str()
+
+    def move(self, loc, destination):
+        path = nx.shortest_path(self.G, loc, destination)
+        path.pop(0)  # First item in path is always where we are
+        while len(path) > 0:
+            next_loc = path.pop(0)
+            direction = self.dir_from_to[(loc, next_loc)]
+            self.send_msg(direction)
+            message = self.execute_str()
+            room_name = self.parse_title(message)
+            assert room_name == next_loc
+            loc = next_loc
+            self.loc = loc
+
+    def try_all_items(self):
+        print("=== Going to Security Checkpoint")
+        destination = "Security Checkpoint"
+        self.move(self.loc, destination)
+        items = self.get_items()
+
+        for item in items:
+            self.send_msg(f"drop {item}")
+
+        for n in range(len(items)):
+            for these_items in list(combinations(items, n)):
+                for item in these_items:
+                    self.send_msg(f"take {item}")
+                self.send_msg("south")
+
+                message = self.execute_str()
+                if self.cpu.is_halted():
+                    print("")
+                    print(f"Correct combination: {these_items}")
+                    print("")
+                    print(message)
+                    return
+
+                for item in these_items:
+                    self.send_msg(f"drop {item}")
+
+    def get_items(self):
+        self.send_msg("inv")
+        message = self.execute_str()
+        items = self.parse_list(message)
+        return items
+
+    def is_blacklisted(self, item):
+        return item in [
+            "infinite loop",
+            "escape pod",
+            "molten lava",
+            "giant electromagnet",
+            "photons",
+        ]
 
     # in: message
-    # out: [('north', -1j), ('south', 1j), ('west', (-1+0j))]
+    # out: ["north", "east", "west"]
     def parse_directions(self, message):
         dirs = []
-        if re.search(r"- east", message):
-            dirs.append(("east", complex(1, 0)))
-        if re.search(r"- north", message):
-            dirs.append(("north", complex(0, -1)))
-        if re.search(r"- south", message):
-            dirs.append(("south", complex(0, 1)))
-        if re.search(r"- west", message):
-            dirs.append(("west", complex(-1, 0)))
+        if re.search(r"- east\n", message):
+            dirs.append("east")
+        if re.search(r"- north\n", message):
+            dirs.append("north")
+        if re.search(r"- south\n", message):
+            dirs.append("south")
+        if re.search(r"- west\n", message):
+            dirs.append("west")
         return dirs
 
     # in: message
     # out: [] or ['tambourine']
+    def parse_list(self, message):
+        return re.findall(r"- (.*?)(?:\n|$)", message)
+
     def parse_items(self, message):
-        return re.findall(f"Items here:(?:\n- (.+))+", message)
+        item_list = re.findall("Items (?:here|in your inventory):\n(.*)\n\n", message)
+        if len(item_list) > 0:
+            return self.parse_list(item_list[0])
+        return []
+
+    # in: message
+    # out: "Hull Breach"
+    def parse_title(self, message):
+        titles = re.findall(f"== (.*?) ==", message)
+        if len(titles) < 1:
+            raise ValueError("Couldn't find title of this room")
+        return titles[0]
 
     def part1(self):
-        # self.explore()
         while True:
             print(self.execute_str())
-            ins = input("command> ")
+            ins = input(
+                "command (w, n, e, s, take item, drop item, or 'solve' to automatically solve)> "
+            )
             if ins == "w":
                 ins = "west"
             elif ins == "e":
@@ -131,17 +218,19 @@ class Day25:
                 ins = "north"
             elif ins == "s":
                 ins = "south"
+            elif ins == "solve":
+                self.explore()
+                self.pick_up_items()
+                self.try_all_items()
+                return
             self.send_msg(ins)
-        return 5
+        return None
 
     def num_to_str(self, results):
         return "".join([chr(s) for s in results])
 
     def string_to_nums(self, string):
         return [ord(s) for s in string]
-
-    def part2(self):
-        return 6
 
 
 def parse(filename):
@@ -153,10 +242,4 @@ if __name__ == "__main__":
     program = parse("../../25/input.txt")
     d25 = Day25(program)
 
-    p1 = d25.part1()
-    print("part 1")
-    print(p1)
-
-    p2 = d25.part2()
-    print("part 2")
-    print(p2)
+    d25.part1()
