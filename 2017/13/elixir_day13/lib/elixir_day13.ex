f6dacee2020d8fc067737363458a427dea169a88
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-28 12:07:31 -0600

    2017/13: Elixir: part 2, brute force
    Takes 2 minutes to run.  There must be a better solution.

--- 2017/13/elixir_day13/lib/elixir_day13.ex
+++ 2017/13/elixir_day13/lib/elixir_day13.ex
@@ -5,6 +5,11 @@ defmodule ElixirDay13 do
   Documentation for ElixirDay13.
   """
 
+  @doc """
+  Advance scanners 1 step.
+  in: layer_map
+  out: layer_map
+  """
   def advance_scanners(layer_map) do
     Map.keys(layer_map)
     |> Enum.reduce(layer_map, fn i, acc ->
@@ -17,11 +22,44 @@ defmodule ElixirDay13 do
     end)
   end
 
+  @doc """
+  Advance scanners N steps.
+  in: layer_map, N
+  out: layer_map
+  """
+  def advance_scanners_n(_layer_map, n) when n < 0,
+    do: raise("Can't advance_scanners_n with negative number")
+
+  def advance_scanners_n(layer_map, n) when n == 0, do: layer_map
+
+  def advance_scanners_n(layer_map, n) do
+    layer_map
+    |> advance_scanners()
+    |> advance_scanners_n(n - 1)
+  end
+
+  @doc """
+  What is the trip severity with no delay?
+  """
   def trip_severity(layer_map) do
+    trip_severity(layer_map, 0)
+  end
+
+  @doc """
+  What is the trip severity with a given delay?
+  """
+  def trip_severity(layer_map, delay) do
     max = Map.keys(layer_map) |> Enum.max()
+    layer_map = advance_scanners_n(layer_map, delay)
     do_trip_severity(layer_map, 0, 0, max)
   end
 
+  @doc """
+  Calculate one step of trip severity.  Our location advances through the network
+  while the scanners move with advance_scanners.  If we're caught, the score
+  increases by depth * location.  Note that getting caught at step 0 adds 0
+  to the score.
+  """
   def do_trip_severity(_layer_map, packet_location, score, max_location)
       when packet_location > max_location do
     score
@@ -32,12 +70,59 @@ defmodule ElixirDay13 do
     caught = scanner_exists && layer_map[packet_location].scanner == 0
     score = if caught, do: score + packet_location * layer_map[packet_location].depth, else: score
     next_layer_map = advance_scanners(layer_map)
+
     do_trip_severity(next_layer_map, packet_location + 1, score, max_location)
+  end
+
+  @doc """
+  Does moving through the map at layer_map get caught?
+  This is like the trip severity function, but it stops simulating as soon
+  as we're caught.
+  """
+  def caught?(layer_map) do
+    max = Map.keys(layer_map) |> Enum.max()
+    do_caught?(layer_map, 0, max)
+  end
+
+  def do_caught?(_layer_map, packet_location, max_location) when packet_location > max_location,
+    do: false
+
+  def do_caught?(layer_map, packet_location, max_location) do
+    scanner_exists = Map.has_key?(layer_map, packet_location)
+    caught = scanner_exists && layer_map[packet_location].scanner == 0
+
+    if caught do
+      true
+    else
+      next_layer_map = advance_scanners(layer_map)
+      do_caught?(next_layer_map, packet_location + 1, max_location)
+    end
+  end
+
+  @doc """
+  What is the lowest delay we can move through the network without being caught?
+  Uses a simulation approach.  Takes 2 minutes on my computer..
+  """
+  def part2(layer_map) do
+    do_part2(layer_map, 0)
+  end
+
+  def do_part2(layer_map, delay) do
+    if not caught?(layer_map) do
+      delay
+    else
+      layer_map = advance_scanners(layer_map)
+      do_part2(layer_map, delay + 1)
+    end
   end
 
   def main do
     Parse.parse_file("../input.txt")
     |> trip_severity()
     |> IO.inspect(label: "Part 1")
+
+    Parse.parse_file("../input.txt")
+    |> part2()
+    |> IO.inspect(label: "Part 2")
   end
 end
--- 2017/13/elixir_day13/test/elixir_day13_test.exs
+++ 2017/13/elixir_day13/test/elixir_day13_test.exs
@@ -32,6 +32,42 @@ defmodule ElixirDay13Test do
     assert advanced_twice[1].scanner == 0
   end
 
+  test "advance_scanners_n" do
+    scanners = %{
+      0 => %{depth: 99, scanner: 0, delta: 1},
+      1 => %{depth: 2, scanner: 0, delta: 1},
+      2 => %{depth: 3, scanner: 0, delta: 1}
+    }
+
+    ## 2
+    got = scanners |> ElixirDay13.advance_scanners_n(2)
+    want = scanners |> ElixirDay13.advance_scanners() |> ElixirDay13.advance_scanners()
+    assert got == want
+
+    ## 3
+    got = scanners |> ElixirDay13.advance_scanners_n(3)
+
+    want =
+      scanners
+      |> ElixirDay13.advance_scanners()
+      |> ElixirDay13.advance_scanners()
+      |> ElixirDay13.advance_scanners()
+
+    assert got == want
+
+    ## 4
+    got = scanners |> ElixirDay13.advance_scanners_n(4)
+
+    want =
+      scanners
+      |> ElixirDay13.advance_scanners()
+      |> ElixirDay13.advance_scanners()
+      |> ElixirDay13.advance_scanners()
+      |> ElixirDay13.advance_scanners()
+
+    assert got == want
+  end
+
   test "Advance_Scanners Reverse Direction" do
     scanners = %{
       0 => %{depth: 3, scanner: 0, delta: 1}
@@ -61,4 +97,27 @@ defmodule ElixirDay13Test do
 
     assert got == want
   end
+
+  test "Part 1" do
+    got =
+      Parse.parse_file("../input.txt")
+      |> ElixirDay13.trip_severity()
+
+    want = 2264
+
+    assert got == want
+  end
+
+  test "Part 2 Examples" do
+    got =
+      Parse.parse_file("../input_small.txt")
+      |> ElixirDay13.part2()
+
+    want = 10
+
+    assert got == want
+  end
+
+  ## Part 2 real. Answer should be 3875838
+  ## Currently takes two minutes, too long for an automated test.
 end
