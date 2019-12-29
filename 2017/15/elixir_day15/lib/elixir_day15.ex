021fc91ce8f31406b25e0935312a7ccd4737243e
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-29 16:28:05 -0600

    2017/15: elixir: part 2

--- 2017/15/elixir_day15/lib/elixir_day15.ex
+++ 2017/15/elixir_day15/lib/elixir_day15.ex
@@ -6,21 +6,15 @@ defmodule ElixirDay15 do
 
   @right_16 65535
 
-  def genA(input) do
-    rem(input * 16807, 2_147_483_647)
-  end
+  ### Functoins for part 1
 
-  def genB(input) do
-    rem(input * 48271, 2_147_483_647)
-  end
+  def genA(input), do: rem(input * 16807, 2_147_483_647)
 
-  def compare(startA, startB, count) do
-    do_compare(startA, startB, count, 0)
-  end
+  def genB(input), do: rem(input * 48271, 2_147_483_647)
 
-  def do_compare(_startA, _startB, 0, score) do
-    score
-  end
+  def compare(startA, startB, count), do: do_compare(startA, startB, count, 0)
+
+  def do_compare(_startA, _startB, 0, score), do: score
 
   def do_compare(startA, startB, count, score) do
     a = genA(startA)
@@ -33,10 +27,40 @@ defmodule ElixirDay15 do
     end
   end
 
+  ### All "2" functions are for part 2
+
+  def genA2(input) do
+    a = genA(input)
+    if rem(a, 4) == 0, do: a, else: genA2(a)
+  end
+
+  def genB2(input) do
+    b = genB(input)
+    if rem(b, 8) == 0, do: b, else: genB2(b)
+  end
+
+  def compare2(startA, startB, count), do: do_compare2(startA, startB, count, 0)
+
+  def do_compare2(_startA, _startB, 0, score), do: score
+
+  def do_compare2(startA, startB, count, score) do
+    a = genA2(startA)
+    b = genB2(startB)
+
+    if (a &&& @right_16) == (b &&& @right_16) do
+      do_compare2(a, b, count - 1, score + 1)
+    else
+      do_compare2(a, b, count - 1, score)
+    end
+  end
+
+  ### Driver
+
   def main do
-    # assert compare(65, 8921, 3) == 3
-    # compare(65, 8921, 40_000_000) == 588
     compare(289, 629, 40_000_000)
     |> IO.inspect(label: "Part 1")
+
+    compare2(289, 629, 5_000_000)
+    |> IO.inspect(label: "Part 2")
   end
 end
--- 2017/15/elixir_day15/test/elixir_day15_test.exs
+++ 2017/15/elixir_day15/test/elixir_day15_test.exs
@@ -2,7 +2,15 @@ defmodule ElixirDay15Test do
   use ExUnit.Case
   doctest ElixirDay15
 
-  test "greets the world" do
-    assert ElixirDay15.hello() == :world
+  test "Examples from problem" do
+    assert ElixirDay15.compare(65, 8921, 3) == 1
+    assert ElixirDay15.compare(65, 8921, 40_000_000) == 588
+    assert ElixirDay15.compare2(65, 8921, 1055) == 0
+    assert ElixirDay15.compare2(65, 8921, 1056) == 1
+  end
+
+  test "Actual problem" do
+    assert ElixirDay15.compare(289, 629, 40_000_000) == 638
+    assert ElixirDay15.compare2(289, 629, 5_000_000) == 343
   end
 end
