54b5408e930733a49d5fd916203716dd9569c553
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-29 10:10:09 -0600

    2017/14: Part 1

--- 2017/14/python_day14/day14.py
+++ 2017/14/python_day14/day14.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 from aoc.day10 import knot_hash
 
+
+def hexify(string):
+    return bin(int(string, 16))[2:]
+
+
+def part1(magic):
+    count = 0
+    for i in range(128):
+        key = magic + "-" + str(i)
+        list_of_strings_hash = [
+            hexify(letter).zfill(4) for letter in list(knot_hash(key))
+        ]
+        for string in list_of_strings_hash:
+            count += sum(1 for letter in list(string) if letter == "1")
+    return count
+
+
 if __name__ == "__main__":
+    # 128 knot hashes
+    # Each hash has 128 bits which correspond to grid squares
+    magic = "hfdlxzhv"
     print("Part1: ")
-    print(knot_hash("hello"))
+    print(part1(magic))
