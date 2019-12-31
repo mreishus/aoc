b4770b83ed3605968626c13b659f1c882b622d05
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-30 20:32:03 -0600

    2017/17: python: part 2
    We were probably supposed to solve this another way...

--- 2017/17/python_day17/day17.py
+++ 2017/17/python_day17/day17.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+from blist import blist
 
-def spin(skip, max_value):
-    nums = [0]
+
+def spin_p1(skip, max_value):
+    nums = blist([0])
     loc = 0
     last_loc = 0
     for i in range(1, max_value + 1):
@@ -13,8 +15,20 @@ def spin(skip, max_value):
     return nums[last_loc + 1]
 
 
+def spin_p2(skip, max_value):
+    nums = blist([0])
+    loc = 0
+    for i in range(1, max_value + 1):
+        loc += 1
+        nums.insert(loc, i)
+        loc = (loc + skip) % len(nums)
+    return nums[1]
+
+
 if __name__ == "__main__":
     print("Example Part 1: ")
-    print(spin(3, 2017))
+    print(spin_p1(3, 2017))
     print("Real Part 1: ")
-    print(spin(304, 2017))
+    print(spin_p1(304, 2017))
+    print("Real Part 2: ")
+    print(spin_p2(304, 50_000_000))
