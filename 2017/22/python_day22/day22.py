a9e507ce75d451155e3df987760ee54fe14e99e8
Matthew Reishus <mreishus@users.noreply.github.com>
2020-01-05 20:49:06 -0600

    2017/22: python: part 2

--- 2017/22/python_day22/day22.py
+++ 2017/22/python_day22/day22.py
@@ -31,15 +31,48 @@ class Day22:
         under = self.grid[self.virus]
         if under == ".":
             # Clean
-            self.direction *= complex(0, -1)  # Turn left
+            self.turn_left()
             self.grid[self.virus] = "#"
             self.infection_count += 1
         elif under == "#":
             # Dirty
-            self.direction *= complex(0, 1)  # Turn right
+            self.turn_right()
             self.grid[self.virus] = "."
         else:
             raise ValueError("Square is neither clean nor dirty")
+        self.virus += self.direction
+
+    def turn_right(self):
+        self.direction *= complex(0, 1)
+
+    def turn_left(self):
+        self.direction *= complex(0, -1)
+
+    def reverse(self):
+        self.turn_right()
+        self.turn_right()
+
+    def step_part2(self):
+        under = self.grid[self.virus]
+        if under == ".":
+            # Clean -> Weak
+            self.turn_left()
+            self.grid[self.virus] = "W"
+        elif under == "W":
+            # Weak -> Infected
+            self.grid[self.virus] = "#"
+            self.infection_count += 1
+        elif under == "#":
+            # Infected -> Flagged
+            self.turn_right()
+            self.grid[self.virus] = "F"
+        elif under == "F":
+            # Flagged -> Clean
+            self.reverse()
+            self.grid[self.virus] = "."
+        else:
+            raise ValueError("Unknown square value")
+        # Step Forward
         self.virus += self.direction
 
     def display(self):
@@ -53,11 +86,16 @@ class Day22:
 
 
 if __name__ == "__main__":
+    print("Part 1:")
     d22 = Day22("../input.txt")
-    print(d22.grid)
-    # d22.display()
     for i in range(10_000):
         d22.step()
     d22.display()
     print("")
+    print(d22.infection_count)
+    print("")
+    print("Part 2:")
+    d22 = Day22("../input.txt")
+    for i in range(10000000):
+        d22.step_part2()
     print(d22.infection_count)
