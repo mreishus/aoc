cf97f3d8704db9a885be85bfc9398b022be77aaf
Matthew Reishus <mreishus@users.noreply.github.com>
2021-12-02 23:25:45 -0600

    2021/03: python: horrible messy version

--- 2021/python2021/aoc/day03.py
+++ 2021/python2021/aoc/day03.py
@@ -3,29 +3,12 @@
 Advent Of Code 2021 Day 03
 https://adventofcode.com/2021/day/3
 """
-from typing import List
-import re
-
-PARSER = re.compile(r"thing (\d+),(\d+), stuff")
+from collections import defaultdict
 
 
-def parse(filename: str) -> List[int]:
+def parse(filename: str):
     with open(filename) as file:
-        return [int(line.strip()) for line in file.readlines()]
-
-
-def parse2(filename):
-    with open(filename) as file:
-        return [parse_line(line.strip()) for line in file.readlines()]
-
-
-def parse_line(line):
-    return line.split(")")
-
-
-def parse_line2(line):
-    (x, y) = re.search(PARSER, line).groups()
-    return (x, y)
+        return [(line.strip()) for line in file.readlines()]
 
 
 class Day03:
@@ -37,12 +20,94 @@ class Day03:
         data = parse(filename)
         if len(data) < 15:
             print(data)
-        return 1
+
+        a = data[0]
+        common = ""
+        leastc = ""
+        for i, z in enumerate(a):
+            seen = defaultdict(int)
+            for item in data:
+                seen[item[i]] += 1
+
+            if seen["0"] > seen["1"]:
+                common += "0"
+                leastc += "1"
+            else:
+                common += "1"
+                leastc += "0"
+        # print(common)
+        gamma = int(common, 2)
+        epi = int(leastc, 2)
+        return gamma * epi
 
     @staticmethod
     def part2(filename: str) -> int:
         """ Given a filename, solve 2021 day 03 part 2 """
         data = parse(filename)
-        if len(data) < 15:
-            print(data)
-        return 1
+        o2 = Day03.part1(filename)
+
+        a = data[0]
+        candidates = set(range(len(data)))
+        # print(candidates)
+
+        lessc = None
+        lesscfull = ""
+        for i, z in enumerate(a):
+            seen = defaultdict(int)
+            for cand in candidates:
+                seen[data[cand][i]] += 1
+
+            if seen["0"] > seen["1"]:
+                lessc = "1"
+            else:
+                lessc = "0"
+
+            to_remove = []
+            for cand in candidates:
+                if data[cand][i] != lessc:
+                    to_remove.append(cand)
+            candidates -= set(to_remove)
+            if len(candidates) == 1:
+                lesscfull = data[list(candidates)[0]]
+                break
+            # print(candidates)
+            # print("--")
+
+        # if len(data) < 15:
+        #     print(data)
+        co2 = int(lesscfull, 2)
+
+        lessc = None
+        lesscfull = ""
+        candidates = set(range(len(data)))
+        for i, z in enumerate(a):
+            seen = defaultdict(int)
+            for cand in candidates:
+                seen[data[cand][i]] += 1
+
+            if seen["0"] > seen["1"]:
+                lessc = "0"
+            else:
+                lessc = "1"
+            print(f" Place {i} Winner {lessc} ")
+
+            to_remove = []
+            for cand in candidates:
+                if data[cand][i] != lessc:
+                    to_remove.append(cand)
+            candidates -= set(to_remove)
+            if len(candidates) == 1:
+                lesscfull = data[list(candidates)[0]]
+                break
+            print(candidates)
+            for cand in candidates:
+                print(data[cand])
+            print("--")
+
+        # if len(data) < 15:
+        #     print(data)
+        o2 = int(lesscfull, 2)
+        print(f"------> {co2} <----")
+        print(f"------> {o2} <----")
+        # (You guessed 3394305.)
+        return co2 * o2
