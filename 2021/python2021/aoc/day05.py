008326ab321552859962de8a5b7a265ef6f9c645
Matthew Reishus <mreishus@users.noreply.github.com>
2021-12-04 23:42:46 -0600

    2021/05: python: cleanup

--- 2021/python2021/aoc/day05.py
+++ 2021/python2021/aoc/day05.py
@@ -3,15 +3,10 @@
 Advent Of Code 2021 Day 05
 https://adventofcode.com/2021/day/5
 """
-from typing import List
 import re
 from collections import defaultdict
 
 PARSER = re.compile("(\d+),(\d+) -> (\d+),(\d+)")
-
-
-def ints(s: str) -> List[int]:
-    return list(map(int, re.findall(r"(?:(?<!\d)-)?\d+", s)))
 
 
 def parse(filename: str):
@@ -24,45 +19,42 @@ def parse_line(line):
     return (int(a), int(b), int(c), int(d))
 
 
-def getrange(a, b):
+def steps(a, b):
+    """
+    steps(5, 9) = 5, 6, 7, 8, 9
+    steps(9, 5) = 9, 8, 7, 6, 5
+    """
+
     if b > a:
-        return list(range(a, b + 1))
+        return range(a, b + 1)
     elif b < a:
-        return list(range(a, b - 1, -1))
-    exit("Fail assert")
-    return []
+        return range(a, b - 1, -1)
+    raise ValueError
 
 
-class Board:
-    def __init__(self, data):
-        grid = defaultdict(int)
-        seen_double = set()
+def get_line_coords(x1, y1, x2, y2, use_45):
+    if x1 == x2:
+        for y in steps(y1, y2):
+            yield (x1, y)
+    elif y1 == y2:
+        for x in steps(x1, x2):
+            yield (x, y1)
+    elif use_45:
+        for (x, y) in zip(steps(x1, x2), steps(y1, y2)):
+            yield (x, y)
 
-        for (x1, y1, x2, y2) in data:
-            # print(f"Consider {x1} {y1} -> {x2} {y2}")
-            if x1 == x2:
-                for y in getrange(y1, y2):
-                    # print(f"Mark {x1} {y}")
-                    grid[x1, y] += 1
-                    if grid[x1, y] > 1:
-                        # print("It's double")
-                        seen_double.add((x1, y))
-            elif y1 == y2:
-                for x in getrange(x1, x2):
-                    # print(f"Mark {x} {y1}")
-                    grid[x, y1] += 1
-                    if grid[x, y1] > 1:
-                        # print("It's double")
-                        seen_double.add((x, y1))
-            else:
-                # print(f"Diag: {x1} {y1} -> {x2} {y2}")
-                for (x, y) in zip(getrange(x1, x2), getrange(y1, y2)):
-                    # print(f"{x} {y}")
-                    grid[x, y] += 1
-                    if grid[x, y] > 1:
-                        seen_double.add((x, y))
 
-        self.double_count = len(seen_double)
+def calculate_overlaps(data, use_diagonal):
+    grid = defaultdict(int)
+    seen_double = set()
+
+    for (x1, y1, x2, y2) in data:
+        for (x, y) in get_line_coords(x1, y1, x2, y2, use_diagonal):
+            grid[x, y] += 1
+            if grid[x, y] > 1:
+                seen_double.add((x, y))
+
+    return len(seen_double)
 
 
 class Day05:
@@ -72,16 +64,10 @@ class Day05:
     def part1(filename: str) -> int:
         """ Given a filename, solve 2021 day 05 part 1 """
         data = parse(filename)
-        if len(data) < 20:
-            print(data)
-        b = Board(data)
-        return b.double_count
-        return -1
+        return calculate_overlaps(data, False)
 
     @staticmethod
     def part2(filename: str) -> int:
         """ Given a filename, solve 2021 day 05 part 2 """
         data = parse(filename)
-        if len(data) < 20:
-            print(data)
-        return -1
+        return calculate_overlaps(data, True)
