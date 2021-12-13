453a6a70c4ec39acceaccbcd67764c5fd540ace9
Matthew Reishus <mreishus@users.noreply.github.com>
2021-12-12 20:42:34 -0600

    2021/12: elixir: refactor

--- 2021/elixir2021/lib/day12.ex
+++ 2021/elixir2021/lib/day12.ex
@@ -15,12 +15,6 @@ defmodule Elixir2021.Day12.State do
     else
       has_double = not Day12.big?(room) and Enum.member?(state.path, room)
       %State{path: state.path ++ [room], doubled_small?: has_double}
-      # has_double =
-      #   state
-      #   |> Enum.filter(fn r -> !big?(r) end)
-      #   |> Enum.frequencies()
-      #   |> Map.values()
-      #   |> Enum.any?(fn x -> x > 1 end)
     end
   end
 end
@@ -43,30 +37,17 @@ defmodule Elixir2021.Day12 do
 
   def search([], count, _config), do: count
 
-  def search(open_set, count, %{routes: routes, get_next: get_next} = config) do
+  def search(open_set, count, routes) do
     complete_count =
       Enum.count(open_set, fn state ->
         List.last(state.path) == "end"
       end)
 
-    Enum.flat_map(open_set, fn state -> get_next.(state, routes) end)
-    |> search(count + complete_count, config)
+    Enum.flat_map(open_set, fn state -> next_states(state, routes) end)
+    |> search(count + complete_count, routes)
   end
 
-  def next_states1(state, routes) do
-    here = List.last(state.path)
-
-    if here == "end" do
-      []
-    else
-      Map.get(routes, here)
-      |> Enum.filter(fn room -> room != "start" end)
-      |> Enum.filter(fn room -> big?(room) || !Enum.member?(state.path, room) end)
-      |> Enum.map(fn room -> State.add_room(state, room) end)
-    end
-  end
-
-  def next_states2(state, routes) do
+  def next_states(state, routes) do
     here = List.last(state.path)
 
     if here == "end" do
@@ -89,13 +70,13 @@ defmodule Elixir2021.Day12 do
 
   def part1(filename) do
     routes = parse(filename)
-    open_set = [State.new(["start"], false)]
-    search(open_set, 0, %{routes: routes, get_next: &next_states1/2})
+    open_set = [State.new(["start"], true)]
+    search(open_set, 0, routes)
   end
 
   def part2(filename) do
     routes = parse(filename)
     open_set = [State.new(["start"], false)]
-    search(open_set, 0, %{routes: routes, get_next: &next_states2/2})
+    search(open_set, 0, routes)
   end
 end
