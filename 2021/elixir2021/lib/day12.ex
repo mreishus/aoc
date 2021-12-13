9c7f1de6616ff7f78573ca9213f9f384904ca948
Matthew Reishus <mreishus@users.noreply.github.com>
2021-12-12 20:40:17 -0600

    2021/12: elixir: refactor

--- 2021/elixir2021/lib/day12.ex
+++ 2021/elixir2021/lib/day12.ex
@@ -1,4 +1,33 @@
+defmodule Elixir2021.Day12.State do
+  alias Elixir2021.Day12
+  alias Elixir2021.Day12.State
+
+  defstruct path: [],
+            doubled_small?: false
+
+  def new(path, doubled_small?) do
+    %State{path: path, doubled_small?: doubled_small?}
+  end
+
+  def add_room(state = %State{}, room) do
+    if state.doubled_small? do
+      %State{path: state.path ++ [room], doubled_small?: true}
+    else
+      has_double = not Day12.big?(room) and Enum.member?(state.path, room)
+      %State{path: state.path ++ [room], doubled_small?: has_double}
+      # has_double =
+      #   state
+      #   |> Enum.filter(fn r -> !big?(r) end)
+      #   |> Enum.frequencies()
+      #   |> Map.values()
+      #   |> Enum.any?(fn x -> x > 1 end)
+    end
+  end
+end
+
 defmodule Elixir2021.Day12 do
+  alias Elixir2021.Day12.State
+
   def parse(filename) do
     File.stream!(filename)
     |> Stream.map(&String.trim/1)
@@ -16,8 +45,8 @@ defmodule Elixir2021.Day12 do
 
   def search(open_set, count, %{routes: routes, get_next: get_next} = config) do
     complete_count =
-      Enum.count(open_set, fn path ->
-        List.last(path) == "end"
+      Enum.count(open_set, fn state ->
+        List.last(state.path) == "end"
       end)
 
     Enum.flat_map(open_set, fn state -> get_next.(state, routes) end)
@@ -25,53 +54,48 @@ defmodule Elixir2021.Day12 do
   end
 
   def next_states1(state, routes) do
-    here = List.last(state)
+    here = List.last(state.path)
 
     if here == "end" do
       []
     else
       Map.get(routes, here)
       |> Enum.filter(fn room -> room != "start" end)
-      |> Enum.filter(fn room -> big?(room) || !Enum.member?(state, room) end)
-      |> Enum.map(fn room -> state ++ [room] end)
+      |> Enum.filter(fn room -> big?(room) || !Enum.member?(state.path, room) end)
+      |> Enum.map(fn room -> State.add_room(state, room) end)
     end
   end
 
   def next_states2(state, routes) do
-    here = List.last(state)
-
-    has_double =
-      state
-      |> Enum.filter(fn r -> !big?(r) end)
-      |> Enum.frequencies()
-      |> Map.values()
-      |> Enum.any?(fn x -> x > 1 end)
+    here = List.last(state.path)
 
     if here == "end" do
       []
     else
       Map.get(routes, here)
       |> Enum.filter(fn room -> room != "start" end)
-      |> Enum.filter(fn room -> big?(room) || small_room_allowed(state, room, has_double) end)
-      |> Enum.map(fn room -> state ++ [room] end)
+      |> Enum.filter(fn room -> big?(room) || small_room_allowed(state, room) end)
+      |> Enum.map(fn room -> State.add_room(state, room) end)
     end
   end
 
-  def small_room_allowed(state, room, true) do
-    !Enum.member?(state, room)
-  end
-
-  def small_room_allowed(state, room, false) do
-    Enum.count(state, fn x -> x == room end) < 2
+  def small_room_allowed(state = %State{}, room) do
+    if state.doubled_small? do
+      !Enum.member?(state.path, room)
+    else
+      Enum.count(state.path, fn x -> x == room end) < 2
+    end
   end
 
   def part1(filename) do
     routes = parse(filename)
-    search([["start"]], 0, %{routes: routes, get_next: &next_states1/2})
+    open_set = [State.new(["start"], false)]
+    search(open_set, 0, %{routes: routes, get_next: &next_states1/2})
   end
 
   def part2(filename) do
     routes = parse(filename)
-    search([["start"]], 0, %{routes: routes, get_next: &next_states2/2})
+    open_set = [State.new(["start"], false)]
+    search(open_set, 0, %{routes: routes, get_next: &next_states2/2})
   end
 end
