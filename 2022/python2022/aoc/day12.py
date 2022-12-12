1e08d824b3b5625e17525d33e6d39be09c78dd30
Matthew Reishus <mreishus@users.noreply.github.com>
2022-12-12 09:33:39 -0600

    2022/12: python: part 2: run bfs backwards

--- 2022/python2022/aoc/day12.py
+++ 2022/python2022/aoc/day12.py
@@ -4,7 +4,6 @@ Advent Of Code 2022 Day 12
 https://adventofcode.com/2022/day/12
 """
 from collections import deque
-from functools import cache
 from typing import NamedTuple
 
 State = NamedTuple("State", [("loc", tuple[int, int]), ("steps", int)])
@@ -20,13 +19,9 @@ class Grid:
         self.aas = []
 
     def parse(self, filename: str):
-        self.max_x = 0
-        x = 0
-        y = 0
-
         with open(filename) as file:
-            for line in file:
-                for char in line.strip():
+            for y, line in enumerate(file):
+                for x, char in enumerate( line.strip() ):
                     self.grid[x, y] = self.fix_elevation(char)
                     if char == "S":
                         self.start = x, y
@@ -35,11 +30,8 @@ class Grid:
                         self.end = x, y
                     elif char == "a":
                         self.aas.append((x, y))
-                    x += 1
-                    self.max_x = max(self.max_x, x)
-                y += 1
-                x = 0
-                self.max_y = max(self.max_y, y)
+                    self.max_x = max(self.max_x, x+1) # eh :(, could be fixed
+                self.max_y = max(self.max_y, y+1) # same
 
     def fix_elevation(self, letter):
         if letter == "S":
@@ -48,7 +40,6 @@ class Grid:
             return "z"
         return letter
 
-    @cache
     def get_raw_neighbors(self, x, y):
         x_size = self.max_x
         y_size = self.max_y
@@ -62,7 +53,6 @@ class Grid:
             if xx >= 0 and xx < x_size and yy >= 0 and yy < y_size:
                 yield xx, yy
 
-    @cache
     def get_neighbors(self, loc):
         x, y = loc
         height = ord(self.grid[x, y])
@@ -71,24 +61,14 @@ class Grid:
             if this_height <= (height + 1):
                 yield xx, yy
 
-    def part2(self):
-        cand = []
-        for aa in self.aas:
-            self.start = aa
-
-            ### These lines kill the performance, but
-            ### they are needed to get the correct answer
-            self.get_neighbors.cache_clear()
-            self.get_raw_neighbors.cache_clear()
-            ### There's a 10x speedup if I learn how they can be removed
-
-            ## Possible performance gain:
-            ## Tell BFS the min number of steps we've already found,
-            ## it can cancel its search if it goes over that number.
-            z = self.bfs()
-            if z is not None:
-                cand.append(z)
-        return min(cand)
+    def get_neighbors_p2(self, loc):
+        """ for running backwards """
+        x, y = loc
+        height = ord(self.grid[x, y])
+        for (xx, yy) in self.get_raw_neighbors(x, y):
+            this_height = ord(self.grid[xx, yy])
+            if height <= (this_height + 1):
+                yield xx, yy
 
     def bfs(self) -> (int | None):
         if self.start is None or self.end is None:
@@ -113,18 +93,42 @@ class Grid:
 
         return None
 
+    def bfs_p2(self) -> (int | None):
+        """ bfs backwards from end point to first 'a' encountered """
+        if self.end is None:
+            return None
+
+        init = State(self.end, 0)
+        queue = deque([init])
+        visited = set()
+        while queue:
+            state = queue.popleft()
+
+            if state.loc in visited:
+                continue
+            visited.add(state.loc)
+
+            if self.grid[state.loc] == "a":
+                return state.steps
+
+            for loc in self.get_neighbors_p2(state.loc):
+                if loc not in visited:
+                    queue.append(State(loc, state.steps + 1))
+
+        return None
+
 
 class Day12:
     """AoC 2022 Day 12"""
 
     @staticmethod
-    def part1(filename: str) -> int:
+    def part1(filename: str) -> (int | None):
         g = Grid()
         g.parse(filename)
         return g.bfs()
 
     @staticmethod
-    def part2(filename: str) -> int:
+    def part2(filename: str) -> (int | None):
         g = Grid()
         g.parse(filename)
-        return g.part2()
+        return g.bfs_p2()
