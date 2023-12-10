9092de288b0796377db04331fd8c234d67eb230e
Matthew Reishus <mreishus@users.noreply.github.com>
2023-12-10 00:31:49 -0600

    2023/10: python: part 2: cleanup

--- 2023/python2023/aoc/day10.py
+++ 2023/python2023/aoc/day10.py
@@ -3,61 +3,17 @@
 Advent Of Code 2023 Day 10
 https://adventofcode.com/2023/day/10
 """
-import re
-from typing import List
 from collections import defaultdict
-
-
-def parse(filename: str):
-    with open(filename) as file:
-        return [parse_line(line.strip()) for line in file.readlines()]
-
-
-def parse_line(line):
-    return line
 
 
 class Grid:
     def __init__(self):
         self.grid = {}
         self.grid = defaultdict(lambda: ".", self.grid)
-        self.distances = {}
 
         self.max_x = 0
         self.max_y = 0
         self.start = (0, 0)
-
-    def parse(self, filename: str):
-        self.max_x = 0
-        x = 0
-        y = 0
-
-        with open(filename) as file:
-            for line in file:
-                for char in line.strip():
-                    self.grid[x, y] = char
-
-                    if char == "S":
-                        self.start = (x, y)
-                        starts = {
-                            "../inputs/10/input_small.txt": "F",
-                            "../inputs/10/input_small2.txt": "F",
-                            "../inputs/10/input_small3.txt": "F",
-                            "../inputs/10/input_small4.txt": "F",
-                            "../inputs/10/input_small5.txt": "F",
-                            "../inputs/10/input_small6.txt": "F",
-                            "../inputs/10/input_small7.txt": "F",
-                            "../inputs/10/input.txt": "|",
-                        }
-                        if filename in starts:
-                            self.grid[x, y] = starts[filename]
-                            print("Overriding start position to", starts[filename])
-
-                    x += 1
-                    self.max_x = max(self.max_x, x)
-                y += 1
-                x = 0
-                self.max_y = max(self.max_y, y)
 
     def parse2(self, filename: str):
         ## Just like parse, except we parse at double the resolution.
@@ -83,7 +39,7 @@ class Grid:
                         }
                         if filename in starts:
                             char = starts[filename]
-                            print("Overriding start position to", starts[filename])
+                            # print("Overriding start position to", starts[filename])
 
                     if char == "|":
                         self.grid[x, y] = "|"
@@ -111,15 +67,13 @@ class Grid:
                 self.max_y = max(self.max_y, y)
 
     def part1(self):
-        max_distance, distances = self.flood(self.start)
+        max_distance, _distances = self.flood(self.start)
         return max_distance // 2
 
     def part2(self):
-        max_distance, loop_distances = self.flood(self.start)
-        # print("Max distance:", max_distance)
-        # print("Loop distances:", loop_distances)
+        _max_distance, loop_distances = self.flood(self.start)
 
-        ## Delete Junk
+        ## Delete Junk - any pipes not in loop
         for x in range(0, self.max_x, 2):
             for y in range(0, self.max_y, 2):
                 all_four = [(x, y), (x + 1, y), (x, y + 1), (x + 1, y + 1)]
@@ -127,13 +81,8 @@ class Grid:
                     for xx, yy in all_four:
                         self.grid[xx, yy] = "."
 
-        outside_max, outside_distances = self.flood((-1, -1))
-        # print("Outside max distance:", outside_max)
-        # print("Outside distances:", outside_distances)
-
-        print("")
-        # self.display2([])
-        self.display2(outside_distances)
+        _outside_max, outside_distances = self.flood((-1, -1))
+        # self.display2(outside_distances)
 
         surrounded = 0
         for x in range(0, self.max_x, 2):
@@ -144,10 +93,9 @@ class Grid:
                     ## Check if all four are in outside_distances
                     if not all([(xx, yy) in outside_distances for xx, yy in all_four]):
                         surrounded += 1
-                        print("Surrounded:", x, y)
         return surrounded
 
-    def display2(self, outside_distances):
+    def display2(self, outside_distances=[]):
         for y in range(self.max_y):
             for x in range(self.max_x):
                 if (x, y) in outside_distances:
@@ -156,23 +104,17 @@ class Grid:
                     print(self.grid[x, y], end="")
             print()
 
-    def flood(self, begin=None, stop_early=999999999999):
+    def flood(self, begin=None):
         q = [(begin, 0)]
         distances = {}
         distances[begin] = 0
 
         max_distance = 0
-        i = 0
         while len(q) > 0:
-            i += 1
-            if i > stop_early:
-                return max_distance, distances
             (x, y), distance = q.pop(0)
             max_distance = max(max_distance, distance)
-            # print(x, y)
             for xx, yy in self.get_neighbours(x, y):
                 if (xx, yy) not in distances:
-                    # print("  ", xx, yy, "==", self.grid[xx, yy], "  ", distance + 1)
                     distances[xx, yy] = distance + 1
                     q.append(((xx, yy), distance + 1))
         return max_distance, distances
@@ -203,19 +145,9 @@ class Grid:
                 if self.grid[a, b] == ".":
                     cands.append((a, b))
         for xx, yy in cands:
-            # if 0 <= xx <= self.max_x and 0 <= yy <= self.max_y:
+            # Let's expand the grid by one
             if -1 <= xx <= (self.max_x + 1) and -1 <= yy <= (self.max_y + 1):
                 yield (xx, yy)
-
-    def display(self):
-        for y in range(self.max_y):
-            for x in range(self.max_x):
-                print(self.grid[x, y], end="")
-            print()
-
-
-def ints(s: str) -> List[int]:
-    return list(map(int, re.findall(r"(?:(?<!\d)-)?\d+", s)))
 
 
 class Day10:
